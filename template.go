e87aa8ae5e9fc8fc14889f943694ee5c7bc92721
Marco <gazerro@open2b.com>
2018-11-13 10:06:03 +0100

    Rimosssa la cache dal tipo "Template"
    Ora la cache è implementata direttamente dal parser.

--- template.go
+++ template.go
@@ -7,8 +7,6 @@ package template
 import (
 	"errors"
 	"io"
-	"sync"
-
 	"open2b/template/ast"
 	"open2b/template/exec"
 	"open2b/template/parser"
@@ -34,23 +32,15 @@ var (
 )
 
 type Template struct {
-	parser *parser.Parser
+	parser       *parser.Parser
 	errorHandler func(error) bool
-	trees  map[string]*ast.Tree
-	sync.RWMutex
 }
 
 // New ritorna un template i cui file vengono letti dalla directory dir.
-// Se cache è true i file vengono letti e parsati una sola volta al
-// momento della loro prima esecuzione.
+// Il parametro cache è deprecato.
 func New(dir string, cache bool) *Template {
-	var trees map[string]*ast.Tree
 	var r parser.Reader = parser.DirReader(dir)
-	if cache {
-		trees = map[string]*ast.Tree{}
-		r = parser.NewCacheReader(r)
-	}
-	return &Template{parser: parser.NewParser(r), trees: trees}
+	return &Template{parser: parser.NewParser(r)}
 }
 
 // Execute esegue il file del template con il path indicato, relativo
@@ -71,29 +61,9 @@ func (t *Template) Execute(out io.Writer, path string, vars interface{}) error {
 	default:
 		ctx = ast.ContextText
 	}
-	var err error
-	var tree *ast.Tree
-	if t.trees == nil {
-		// senza cache
-		tree, err = t.parser.Parse(path, ctx)
-		if err != nil {
-			return convertError(err)
-		}
-	} else {
-		// con cache
-		t.RLock()
-		tree = t.trees[path]
-		t.RUnlock()
-		if tree == nil {
-			// parsa l'albero di path e crea l'ambiente
-			tree, err = t.parser.Parse(path, ctx)
-			if err != nil {
-				return convertError(err)
-			}
-			t.Lock()
-			t.trees[path] = tree
-			t.Unlock()
-		}
+	tree, err := t.parser.Parse(path, ctx)
+	if err != nil {
+		return convertError(err)
 	}
 	return exec.Execute(out, tree, "", vars, t.errorHandler)
 }
