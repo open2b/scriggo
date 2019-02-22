69f77da6761097fd5375ff556a75cc90f1e13675
Gianluca Mondini <zapateo@live.it>
2019-02-22 12:01:41 +0100

    Aggiunge ulteriori dichiarazioni al test delle costanti

--- examples/odd/const-declaration.go
+++ examples/odd/const-declaration.go
@@ -21,4 +21,19 @@ func main() {
 	fmt.Println(a, b, c, d, e)
 	fmt.Println(f, g, h, i, j)
 	fmt.Println(k, l)
+	const a = 1
+	const b, c = 2, 3
+	const (
+		d    = 4
+		e, f = 5, 6
+	)
+	const g int = 7
+	const h, i int = 8, 9
+	const (
+		j    int = 10
+		k, l int = 11, 12
+	)
+	fmt.Println(a, b, c, d, e)
+	fmt.Println(f, g, h, i, j)
+	fmt.Println(k, l)
 }
