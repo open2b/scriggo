49760740eb2d3772322d6d3ca4dda8b96bf452ee
Gianluca Mondini <zapateo@live.it>
2019-03-05 15:40:59 +0100

    Apporta modifiche minori all'assegnamento

--- parser/checker_statements.go
+++ parser/checker_statements.go
@@ -11,12 +11,15 @@ import (
 	"scrigo/ast"
 )
 
+// checkNodesInNewScope checks nodes in a dedicated scope, which will be
+// destroyed after use.
 func (tc *typechecker) checkNodesInNewScope(nodes []ast.Node) {
 	tc.AddScope()
 	tc.checkNodes(nodes)
 	tc.RemoveCurrentScope()
 }
 
+// checkNodes checks nodes an orderer list of statements.
 func (tc *typechecker) checkNodes(nodes []ast.Node) {
 
 	for _, node := range nodes {
@@ -171,7 +174,9 @@ func (tc *typechecker) checkNodes(nodes []ast.Node) {
 
 }
 
-// checkCase checks a switch or type-switch case.
+// checkCase checks a switch or type-switch case. isTypeSwitch indicates if the
+// parent switch is a type-switch. switchExpr contains the expression of the
+// parent switch.
 func (tc *typechecker) checkCase(node *ast.Case, isTypeSwitch bool, switchExpr ast.Expression) error {
 	tc.AddScope()
 	switchExprTyp := tc.typeof(switchExpr, noEllipses)
@@ -193,54 +198,77 @@ func (tc *typechecker) checkCase(node *ast.Case, isTypeSwitch bool, switchExpr a
 	return nil
 }
 
+// assignValueToVariable generically assigns value to variable. node must
+// contain the assignment node (or the var/const declaration node) and it's used
+// for error messages only. If the declaration specified a type, that must be
+// passed as "typ" argument. isDeclaration and isConst indicates, respectively,
+// if the assignment is a declaration and if it's a constant.
+//
 // TODO (Gianluca): handle "isConst"
 //
-// TODO (Gianluca): typ doesn't get the type zero, just checks if type is
+// TODO (Gianluca): typ doesn't get the type's zero, just checks if type is
 // correct when a value is provided. Implement "var a int"
-//
-// TODO (Gianluca): AssignScope must be called always, because constant value
-// inside scope must be updated.
 //
 // TODO (Gianluca):when assigning a costant to a value in scope, constant isn't
 // constant anymore.
+//
 func (tc *typechecker) assignValueToVariable(node ast.Node, variable, value ast.Expression, typ *ast.TypeInfo, isDeclaration, isConst bool) {
+
+	if isConst && !isDeclaration {
+		panic("bug: cannot have a constant assignment outside a declaration")
+	}
+
 	valueTi := tc.checkExpression(value)
+
+	// If it's a constant declaration, a constant value must be provided.
 	if isConst && (valueTi.Constant == nil) {
 		panic(tc.errorf(node, "const initializer %s is not a constant", value))
 	}
+
+	// If a type is provided, value must be assignable to type.
 	if typ != nil && !tc.isAssignableTo(valueTi, typ.Type) {
 		panic(tc.errorf(node, "canont use %v (type %v) as type %v in assignment", value, valueTi, typ))
 	}
+
 	switch v := variable.(type) {
+
 	case *ast.Identifier:
-		_, alreadyDefinedInCurrentScope := tc.LookupScope(v.Name, true)
-		if isDeclaration && alreadyDefinedInCurrentScope {
+
+		_, alreadyInCurrentScope := tc.LookupScope(v.Name, true)
+
+		// Cannot declarate a variable if already exists in current scope.
+		if isDeclaration && alreadyInCurrentScope {
 			panic(tc.errorf(node, "no new variables on left side of :="))
 		}
+
+		// If it's not a declaration, variable must already exists in some
+		// scope. Its type must be retrieved, and value must be assignable
+		// to that.
+		variableTi := tc.checkExpression(variable)
 		if !isDeclaration {
-			variableTi := tc.checkExpression(variable)
-			{ // Inizio parte da rimuovere (usata per il DEBUG, TODO (Gianluca): rimuovere.)
-				if variableTi.Type == nil {
-					panic("non ho una dichiarazione ma variableTi.Type è comunque nil..")
-				}
-			} // Fine parte da rimuovere
 			if !tc.isAssignableTo(valueTi, variableTi.Type) {
 				panic(tc.errorf(node, "cannot use %v (type %v) as type %v in assignment", value, valueTi.Type, variableTi.Type))
 			}
 		}
+
 		newValueTi := &ast.TypeInfo{}
 		if isDeclaration {
 			defaultType := tc.concreteType(valueTi)
 			newValueTi.Type = defaultType
 		}
 		tc.AssignScope(v.Name, newValueTi)
+
 	default:
 		panic("bug/not implemented") // TODO (Gianluca): can we have a declaration without an identifier?
 	}
 	return
 }
 
+// checkAssignmentWithCall checks an assignment where left value is a function
+// call or a convertion.
+//
 // TODO (Gianluca): handle builtin functions.
+//
 func (tc *typechecker) checkAssignmentWithCall(node ast.Node, variables []ast.Expression, call *ast.Call, typ *ast.TypeInfo, isDeclaration, isConst bool) {
 	values := tc.checkCallExpression(call, false) // TODO (Gianluca): is "false" correct?
 	if len(variables) != len(values) {
@@ -252,6 +280,7 @@ func (tc *typechecker) checkAssignmentWithCall(node ast.Node, variables []ast.Ex
 	}
 }
 
+// checkAssignment checks the node assignment.
 // TODO (Gianluca): handle
 //		 var a, b int = f() // func f() (int, string)
 // (should be automatically handled, verify)
@@ -279,6 +308,8 @@ func (tc *typechecker) checkAssignment(node ast.Node) {
 		variables = n.Variables
 		values = n.Values
 		isDeclaration = n.Type == ast.AssignmentDeclaration
+	default:
+		panic(fmt.Errorf("bug: unexpected node %T", node))
 	}
 	if len(variables) == 1 && len(values) == 1 {
 		tc.assignValueToVariable(node, variables[0], values[0], typ, isDeclaration, isConst)
