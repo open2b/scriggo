fc075ab94986ff6755c6cbcf74a7744a1504797f
Marco Gazerro <gazerro@open2b.com>
2019-03-21 10:06:59 +0100

    Ordinata alfabeticamente la funzione rangeOver nel file

--- parser/checker_util.go
+++ parser/checker_util.go
@@ -303,30 +303,6 @@ func isComparison(op ast.OperatorType) bool {
 	return op >= ast.OperatorEqual && op <= ast.OperatorGreaterOrEqual
 }
 
-// rangeOver returns the pair of elements (key, value) obtained from the
-// iteration over typ, if exist.
-func rangeOver(typ reflect.Type) (reflect.Type, reflect.Type, bool) {
-	switch typ.Kind() {
-	case reflect.Array, reflect.Slice:
-		return intType, typ.Elem(), true
-	case reflect.Map:
-		return typ.Key(), typ.Elem(), true
-	case reflect.String:
-		return intType, reflect.TypeOf(rune(' ')), true
-	case reflect.Ptr:
-		if typ.Elem().Kind() != reflect.Array {
-			return nil, nil, false
-		}
-		return intType, typ.Elem().Elem(), true
-	case reflect.Chan:
-		// TODO (Gianluca): «...or channel permitting receive operations...»
-		// Use reflect.Type.ChanDir()
-		return nil, nil, false
-	default:
-		return nil, nil, false
-	}
-}
-
 // isOrdered reports whether t is ordered.
 func isOrdered(t *ast.TypeInfo) bool {
 	k := t.Type.Kind()
@@ -372,6 +348,30 @@ func methodByName(t *ast.TypeInfo, name string) (*ast.TypeInfo, bool) {
 		}
 	}
 	return nil, false
+}
+
+// rangeOver returns the pair of elements (key, value) obtained from the
+// iteration over typ, if exist.
+func rangeOver(typ reflect.Type) (reflect.Type, reflect.Type, bool) {
+	switch typ.Kind() {
+	case reflect.Array, reflect.Slice:
+		return intType, typ.Elem(), true
+	case reflect.Map:
+		return typ.Key(), typ.Elem(), true
+	case reflect.String:
+		return intType, reflect.TypeOf(rune(' ')), true
+	case reflect.Ptr:
+		if typ.Elem().Kind() != reflect.Array {
+			return nil, nil, false
+		}
+		return intType, typ.Elem().Elem(), true
+	case reflect.Chan:
+		// TODO (Gianluca): «...or channel permitting receive operations...»
+		// Use reflect.Type.ChanDir()
+		return nil, nil, false
+	default:
+		return nil, nil, false
+	}
 }
 
 // representedBy returns a constant value represented as a value of type t2.
