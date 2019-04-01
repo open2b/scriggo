5e8d01fa2cd181b446c61eb4333523e6ce23b07c
Gianluca Mondini <zapateo@live.it>
2019-04-01 11:22:09 +0200

    Aggiunge un test di confronto (disabilitato)

---
+++ test/sources/misc/_ignore_ variables-declarations.go
@@ -0,0 +1,20 @@
+//+build ignore
+
+package main
+
+var (
+	A    = 1
+	B, C = 2, 3
+	D    int
+	E    *int
+)
+
+func main() {
+	var (
+		A    = 1
+		B, C = 2, 3
+		D    int
+		E    *int
+	)
+	_, _, _, _, _ = A, B, C, D, E
+}
