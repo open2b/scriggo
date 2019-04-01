1e281fd23fb9081eb05abaf0cac032619e135f79
Gianluca Mondini <zapateo@live.it>
2019-04-01 14:37:37 +0200

    Aggiunge un benchmark per il tool di generazione di packages

---
+++ tools/scrigo-generate/generate_package_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func BenchmarkGeneratePackage(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		v, err := goPackageToDeclarations("fmt")
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = v
+	}
+}
