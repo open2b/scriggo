08b273b1067be8785ff37d122c69c123386b0025
Gianluca Mondini <zapateo@live.it>
2019-04-10 11:02:34 +0200

    Apporta modifiche ai test sulla vm

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -12,42 +12,53 @@ type reg struct {
 	value interface{}
 }
 
-var cases = []struct {
-	src      string
-	expected []reg
-}{
+var cases = map[string][]reg{
 
-	{"a := 10; _ = a",
-		[]reg{
-			{TypeInt, 0, int64(10)},
-		}},
-
-	{"a := 5; b := 10; c := a + b; _ = c",
-		[]reg{
-			{TypeInt, 0, int64(5)},
-			{TypeInt, 1, int64(10)},
-			{TypeInt, 2, int64(15)},
-		}},
+	`a := 10; _ = a`: []reg{
+		{TypeInt, 0, int64(10)}, // a
+	},
+	`a := 5; b := 10; c := a + b; _ = c`: []reg{
+		{TypeInt, 0, int64(5)},  // a
+		{TypeInt, 1, int64(10)}, // b
+		{TypeInt, 2, int64(15)}, // c
+	},
+	`a := 10; c := 0; if a > 5 { c = 1 } else { c = 2 }; _ = c`: []reg{
+		{TypeInt, 0, int64(10)}, // a
+		{TypeInt, 1, int64(1)},  // c
+	},
+	`a := 10; c := 0; if a <= 5 { c = 1 } else { c = 2 }; _ = c`: []reg{
+		{TypeInt, 0, int64(10)}, // a
+		{TypeInt, 1, int64(2)},  // c
+	},
+	`a := 10 + 20 - 4; _ = a`: []reg{
+		{TypeInt, 0, int64(26)}, // a
+	},
+	`c := 0; if x := 1; x == 1 { c = 1 } else { c = 2 }; _ = c`: []reg{
+		{TypeInt, 0, int64(1)}, // c
+	},
+	// `a := "s"; _ = a`: []reg{
+	// 	{TypeString, 0, "s"},
+	// },
 }
 
 func TestVM(t *testing.T) {
 	DebugTraceExecution = false
-	for _, c := range cases {
-		fullSrc := "package main\nfunc main(){\n" + c.src + "\n}\n"
+	for src, registers := range cases {
+		fullSrc := "package main\nfunc main(){\n" + src + "\n}\n"
 		r := parser.MapReader{"/test.go": []byte(fullSrc)}
 		comp := NewCompiler(r, nil)
 		pkg, err := comp.Compile("/test.go")
 		if err != nil {
-			t.Errorf("source: %q, compiler error: %s", c.src, err)
+			t.Errorf("source: %q, compiler error: %s", src, err)
 			continue
 		}
 		vm := New(pkg)
 		_, err = vm.Run("main")
 		if err != nil {
-			t.Errorf("source: %q, execution error: %s", c.src, err)
+			t.Errorf("source: %q, execution error: %s", src, err)
 			continue
 		}
-		for _, reg := range c.expected {
+		for _, reg := range registers {
 			var got interface{}
 			switch reg.typ {
 			case TypeFloat:
@@ -60,7 +71,7 @@ func TestVM(t *testing.T) {
 				got = vm.string(reg.r)
 			}
 			if !reflect.DeepEqual(reg.value, got) {
-				t.Errorf("source %q, register %s[%d]: expecting %v (type %T), got %v (type %T)", c.src, reg.typ, reg.r, reg.value, reg.value, got, got)
+				t.Errorf("source %q, register %s[%d]: expecting %#v (type %T), got %#v (type %T)", src, reg.typ, reg.r, reg.value, reg.value, got, got)
 			}
 		}
 	}
