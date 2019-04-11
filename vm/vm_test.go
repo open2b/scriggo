4fcfff2ba9bd44fac5a62e6ded6aed818ff26345
Gianluca Mondini <zapateo@live.it>
2019-04-11 12:02:25 +0200

    Apporta modifiche ai test sulla vm

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -19,70 +19,118 @@ func ptrTo(v interface{}) interface{} {
 	return rv.Interface()
 }
 
-var stmt_tests = map[string][]reg{
+var stmtTests = []struct {
+	src       string
+	registers []reg
+}{
 
-	`a := 10; _ = a`: []reg{
-		{TypeInt, 0, int64(10)}, // a
+	{
+		`a := 10; _ = a`,
+		[]reg{
+			{TypeInt, 0, int64(10)}, // a
+		},
 	},
-	// `a := 1234; _ = a`: []reg{
+	// `a := 1234; _ = a`,
+	// reg{
 	// 	{TypeInt, 0, int64(1234)}, // a
 	// },
-	`a := 5; b := 10; c := a + b; _ = c`: []reg{
-		{TypeInt, 0, int64(5)},  // a
-		{TypeInt, 1, int64(10)}, // b
-		{TypeInt, 2, int64(15)}, // c
+	{
+		`a := 5; b := 10; c := a + b; _ = c`,
+		[]reg{
+			{TypeInt, 0, int64(5)},  // a
+			{TypeInt, 1, int64(10)}, // b
+			{TypeInt, 2, int64(15)}, // c
+		},
 	},
-	`a := 10; c := 0; if a > 5 { c = 1 } else { c = 2 }; _ = c`: []reg{
-		{TypeInt, 0, int64(10)}, // a
-		{TypeInt, 1, int64(1)},  // c
+	{
+		`a := 10; c := 0; if a > 5 { c = 1 } else { c = 2 }; _ = c`,
+		[]reg{
+			{TypeInt, 0, int64(10)}, // a
+			{TypeInt, 1, int64(1)},  // c
+		},
 	},
-	`a := 10; c := 0; if a <= 5 { c = 1 } else { c = 2 }; _ = c`: []reg{
-		{TypeInt, 0, int64(10)}, // a
-		{TypeInt, 1, int64(2)},  // c
+	{
+		`a := 10; c := 0; if a <= 5 { c = 1 } else { c = 2 }; _ = c`,
+		[]reg{
+			{TypeInt, 0, int64(10)}, // a
+			{TypeInt, 1, int64(2)},  // c
+		},
 	},
-	`a := 10 + 20 - 4; _ = a`: []reg{
-		{TypeInt, 0, int64(26)}, // a
+	{
+		`a := 10 + 20 - 4; _ = a`,
+		[]reg{
+			{TypeInt, 0, int64(26)}, // a
+		},
 	},
-	`c := 0; if x := 1; x == 1 { c = 1 } else { c = 2 }; _ = c`: []reg{
-		{TypeInt, 0, int64(1)}, // c
+	{
+		`c := 0; if x := 1; x == 1 { c = 1 } else { c = 2 }; _ = c`,
+		[]reg{
+			{TypeInt, 0, int64(1)}, // c
+		},
 	},
-	`a := "s"; _ = a; b := "ee" + "ff"; _ = b`: []reg{
-		{TypeString, 0, "s"},    // a
-		{TypeString, 2, "eeff"}, // b
+	{
+		`a := "s"; _ = a; b := "ee" + "ff"; _ = b`,
+		[]reg{
+			{TypeString, 0, "s"},    // a
+			{TypeString, 2, "eeff"}, // b
+		},
 	},
-	`a := []int{}; _ = a`: []reg{
-		{TypeIface, 0, []int{}}, // a
+	{
+		`a := []int{}; _ = a`,
+		[]reg{
+			{TypeIface, 0, []int{}}, // a
+		},
 	},
-	// `a := new(int); _ = a`: []reg{
+	// `a := new(int); _ = a`,
+	// reg{
 	// 	{TypeIface, 0, ptrTo(int64(0))},
 	// },
-	`a := []string{}; _ = a`: []reg{
-		{TypeIface, 0, []string{}}, // a
+	{
+		`a := []string{}; _ = a`,
+		[]reg{
+			{TypeIface, 0, []string{}}, // a
+		},
 	},
-	`a := []int{}; b := []byte{}; _ = a; _ = b`: []reg{
-		{TypeIface, 0, []int{}},  // a
-		{TypeIface, 1, []byte{}}, // b
+	{
+		`a := []int{}; b := []byte{}; _ = a; _ = b`,
+		[]reg{
+			{TypeIface, 0, []int{}},  // a
+			{TypeIface, 1, []byte{}}, // b
+		},
 	},
-	// `a := []int{1,2,4}; _ = a`: []reg{
+	// `a := []int{1,2,4}; _ = a`,
+	// reg{
 	// 	{TypeIface, 0, []int{1, 2, 5}},
 	// },
-	`a := len("abc"); _ = a`: []reg{
-		{TypeInt, 0, int64(3)}, // a
+	{
+		`a := len("abc"); _ = a`,
+		[]reg{
+			{TypeInt, 0, int64(3)}, // a
+		},
 	},
-	`a := 0; switch 1 + 1 { case 1: a = 10 ; case 2: a = 20; case 3: a = 30 }; _ = a`: []reg{
-		{TypeInt, 0, int64(20)},
+	{
+		`a := 0; switch 1 + 1 { case 1: a = 10 ; case 2: a = 20; case 3: a = 30 }; _ = a`,
+		[]reg{
+			{TypeInt, 0, int64(20)},
+		},
 	},
-	`a := 0; switch 1 + 1 { case 1: a = 10 ; case 2: a = 20; fallthrough; case 3: a = 30 }; _ = a`: []reg{
-		{TypeInt, 0, int64(30)},
+	{
+		`a := 0; switch 1 + 1 { case 1: a = 10 ; case 2: a = 20; fallthrough; case 3: a = 30 }; _ = a`,
+		[]reg{
+			{TypeInt, 0, int64(30)},
+		},
 	},
-	// `a := 0; switch 2 + 2 { case 1: a = 10 ; default: a = 80; case 2: a = 20; case 3: a = 30 }; _ = a`: []reg{
+	// `a := 0; switch 2 + 2 { case 1: a = 10 ; default: a = 80; case 2: a = 20; case 3: a = 30 }; _ = a`,
+	// reg{
 	// 	{TypeInt, 0, int64(80)},
 	// },
 }
 
 func TestVM(t *testing.T) {
 	DebugTraceExecution = false
-	for src, registers := range stmt_tests {
+	for _, cas := range stmtTests {
+		src := cas.src
+		registers := cas.registers
 		if src != `a := new(int); _ = a` {
 			continue
 		}
