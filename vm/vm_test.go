8c279ef178a2fe9e52162e7b8c595c5337d58f2d
Gianluca Mondini <zapateo@live.it>
2019-04-17 11:32:32 +0200

    Aggiunge test sulla short circuit evaluation

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -359,6 +359,35 @@ var stmtTests = []struct {
 		},
 	},
 
+	{"Short-circuit evaluation",
+		`
+		package main
+
+		func p() bool {
+			panic("error")
+		}
+
+		func main() {
+			T := true
+			F := false
+
+			v := false
+
+			v = F && p()
+			v = T || p()
+			v = (F || F) && p()
+			v = (T && T) || p()
+			v = F && p() && p()
+			v = F || T || p()
+			v = T && F && p()
+
+			_ = v
+		}
+		`,
+		nil,
+		nil,
+	},
+
 	{"Operator ! (not)",
 		`
 		package main
