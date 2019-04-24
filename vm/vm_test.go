b2816ddf2b9b0d1670dbea718c03ee607211bee6
Gianluca Mondini <zapateo@live.it>
2019-04-24 17:25:34 +0200

    Aggiunge test (commentato) sulla defer

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -140,6 +140,57 @@ var stmtTests = []struct {
 	// 	}
 	// 	`, nil, nil, ""},
 
+	// TODO (Gianluca):
+	// {"Defer",
+	// 	`
+	// 	package main
+
+	// 	import "fmt"
+
+	// 	func mark(m string) {
+	// 		fmt.Print(m + ",")
+	// 	}
+
+	// 	func f() {
+	// 		mark("f")
+	// 		defer func() {
+	// 			mark("f.1")
+	// 			defer func() {
+
+	// 			}()
+	// 		}()
+	// 		defer func() {
+	// 			mark("f.2")
+	// 			defer func() {
+	// 				mark("f.2.1")
+	// 			}()
+	// 			h()
+	// 		}()
+	// 		g()
+	// 		mark("ret f")
+	// 	}
+
+	// 	func g() {
+	// 		mark("g")
+	// 	}
+
+	// 	func h() {
+	// 		mark("h")
+	// 	}
+
+	// 	func main() {
+	// 		mark("main")
+	// 		defer func() {
+	// 			mark("main.1")
+	// 			h()
+	// 			mark("main.1")
+	// 		}()
+	// 		f()
+	// 		mark("ret main")
+	// 	}
+	// 	`,
+	// 	nil, nil, "main,f,g,ret f,f.2,h,f.2.1,f.1,ret main,main.1,h,main.1,"},
+
 	// Assignments.
 
 	{"Single assignment",
