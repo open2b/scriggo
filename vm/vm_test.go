87cc4589304dda67935a492c6ccc9f35c63e8d2f
Gianluca Mondini <zapateo@live.it>
2019-04-26 10:26:07 +0200

    Aggiunge un test sul corretto ordine di valutazione delle funzioni

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -130,6 +130,46 @@ var stmtTests = []struct {
 		},
 		"10 42 88\n"},
 
+	{"Order of evaluation - Composite literal values",
+		`
+		package main
+
+		import (
+			"fmt"
+		)
+
+		func a() int {
+			fmt.Print("a")
+			return 0
+		}
+
+		func b() int {
+			fmt.Print("b")
+			return 0
+		}
+		func c() int {
+			fmt.Print("c")
+			return 0
+		}
+		func d() int {
+			fmt.Print("d")
+			return 0
+		}
+		func e() int {
+			fmt.Print("e")
+			return 0
+		}
+
+		func main() {
+			s := []int{a(), b(), c(), d(), e()}
+			_ = s
+		}
+		`,
+		nil,
+		nil,
+		"abcde",
+	},
+
 	// TODO (Gianluca):
 	// {"Operator address (&)",
 	// 	`
