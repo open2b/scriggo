55cf6db573edbc8f461ce1a879b06762505cde82
Marco <gazerro@open2b.com>
2019-05-05 17:06:26 +0200

    Spostato String sugli operatori da vm.go a disassembler.go
    Solo al disassembler importa il nome delle operazioni.

--- vm/disassembler.go
+++ vm/disassembler.go
@@ -570,3 +570,173 @@ func disassembleOperand(fn *ScrigoFunction, op int8, kind Kind, constant bool) s
 	}
 	return "(" + label + strconv.Itoa(-int(op)) + ")"
 }
+
+func (op operation) String() string {
+	return operationName[op]
+}
+
+var operationName = [...]string{
+
+	opNone: "Nop", // TODO(Gianluca): review.
+
+	opAddInt:     "Add",
+	opAddInt8:    "Add8",
+	opAddInt16:   "Add16",
+	opAddInt32:   "Add32",
+	opAddFloat32: "Add32",
+	opAddFloat64: "Add",
+
+	opAnd: "And",
+
+	opAndNot: "AndNot",
+
+	opAppend: "Append",
+
+	opAppendSlice: "AppendSlice",
+
+	opAssert:        "Assert",
+	opAssertInt:     "Assert",
+	opAssertFloat64: "Assert",
+	opAssertString:  "Assert",
+
+	opBind: "Bind",
+
+	opCall: "Call",
+
+	opCallIndirect: "CallIndirect",
+
+	opCallNative: "CallNative",
+
+	opCap: "Cap",
+
+	opContinue: "Continue",
+
+	opConvert:       "Convert",
+	opConvertInt:    "Convert",
+	opConvertUint:   "ConvertU",
+	opConvertFloat:  "Convert",
+	opConvertString: "Convert",
+
+	opCopy: "Copy",
+
+	opConcat: "concat",
+
+	opDefer: "Defer",
+
+	opDelete: "delete",
+
+	opDivInt:     "Div",
+	opDivInt8:    "Div8",
+	opDivInt16:   "Div16",
+	opDivInt32:   "Div32",
+	opDivUint8:   "DivU8",
+	opDivUint16:  "DivU16",
+	opDivUint32:  "DivU32",
+	opDivUint64:  "DivU64",
+	opDivFloat32: "Div32",
+	opDivFloat64: "Div",
+
+	opFunc: "Func",
+
+	opGetFunc: "GetFunc",
+
+	opGetVar: "GetVar",
+
+	opGo: "Go",
+
+	opGoto: "Goto",
+
+	opIf:       "If",
+	opIfInt:    "If",
+	opIfUint:   "IfU",
+	opIfFloat:  "If",
+	opIfString: "If",
+
+	opIndex: "Index",
+
+	opLen: "Len",
+
+	opMakeChan: "MakeChan",
+
+	opMakeMap: "MakeMap",
+
+	opMakeSlice: "MakeSlice",
+
+	opMapIndex: "MapIndex",
+
+	opMapIndexStringBool: "MapIndexStringBool",
+
+	opMapIndexStringInt: "MapIndexStringInt",
+
+	opMapIndexStringInterface: "MapIndexStringInterface",
+
+	opMapIndexStringString: "MapIndexStringString",
+
+	opMove: "Move",
+
+	opMulInt:     "Mul",
+	opMulInt8:    "Mul8",
+	opMulInt16:   "Mul16",
+	opMulInt32:   "Mul32",
+	opMulFloat32: "Mul32",
+	opMulFloat64: "Mul",
+
+	opNew: "New",
+
+	opOr: "Or",
+
+	opPanic: "Panic",
+
+	opPrint: "Print",
+
+	opRange: "Range",
+
+	opRangeString: "Range",
+
+	opReceive: "Receive",
+
+	opRecover: "Recover",
+
+	opRemInt:    "Rem",
+	opRemInt8:   "Rem8",
+	opRemInt16:  "Rem16",
+	opRemInt32:  "Rem32",
+	opRemUint8:  "RemU8",
+	opRemUint16: "RemU16",
+	opRemUint32: "RemU32",
+	opRemUint64: "RemU64",
+
+	opReturn: "Return",
+
+	opSelector: "Selector",
+
+	opSend: "Send",
+
+	opSetMap: "SetMap",
+
+	opSetSlice: "SetSlice",
+
+	opSetVar: "SetPackageVar",
+
+	opSliceIndex: "SliceIndex",
+
+	opStringIndex: "StringIndex",
+
+	opSubInt:     "Sub",
+	opSubInt8:    "Sub8",
+	opSubInt16:   "Sub16",
+	opSubInt32:   "Sub32",
+	opSubFloat32: "Sub32",
+	opSubFloat64: "Sub",
+
+	opSubInvInt:     "SubInv",
+	opSubInvInt8:    "SubInv8",
+	opSubInvInt16:   "SubInv16",
+	opSubInvInt32:   "SubInv32",
+	opSubInvFloat32: "SubInv32",
+	opSubInvFloat64: "SubInv",
+
+	opTailCall: "TailCall",
+
+	opXor: "Xor",
+}
