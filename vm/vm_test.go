c33ada6811d2fe3912d90e64f39e4a508860e037
Gianluca Mondini <zapateo@live.it>
2019-05-06 15:00:10 +0200

    Aggiunge un test su valori float restituiti da una funzione Scrigo

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -1459,6 +1459,26 @@ var stmtTests = []struct {
 		// },
 		""},
 
+	{"Package function - 'floats'",
+		`package main
+
+		import (
+			"fmt"
+		)
+		
+		func floats() (float32, float64) {
+			a := float32(5.6)
+			b := float64(-432.12)
+			return a, b
+		}
+		
+		func main() {
+			f1, f2 := floats()
+			fmt.Println(f1, f2)
+		}`,
+		nil, nil, "5.6 -432.12\n",
+	},
+
 	// {"Package function with many return values (same type)",
 	// TODO(Gianluca): panics.
 	// 	`package main
