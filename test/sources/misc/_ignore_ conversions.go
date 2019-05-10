8268ab70aaa01dc8a0bf69536e36a505ef47bfc2
Gianluca Mondini <zapateo@live.it>
2019-05-10 16:52:00 +0200

    test: disabilita i test di confronto non funzionanti

---
+++ test/sources/misc/_ignore_ conversions.go
@@ -0,0 +1,19 @@
+//+build ignore
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+
+	m := map[string]interface{}{}
+	m1 := map[string]interface{}(m)
+	_ = m1
+}
---
+++ test/sources/misc/_ignore_ global_functions_scope.go
@@ -0,0 +1,15 @@
+//+build ignore
+
+package main
+
+var A string = "A"
+
+func F() int {
+	A := 10
+	return A
+}
+
+var B = F()
+
+func main() {
+}
