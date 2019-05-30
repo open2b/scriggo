87413f9878cbb2faaf6eb61b40f7c32aa44f8f78
Gianluca Mondini <zapateo@live.it>
2019-05-30 16:14:54 +0200

    test: change packages from "scrigo_test"/"scrigo" to "test"

--- test/example_test.go
+++ test/example_test.go
@@ -9,7 +9,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package scrigo_test
+package test
 
 import (
 	"log"
--- test/script_test.go
+++ test/script_test.go
@@ -1,4 +1,4 @@
-package scrigo
+package test
 
 import (
 	"bytes"
