74bbbd67b2b4da6804a0bd4016f49cf6affc7c3a
Marco Gazerro <gazerro@open2b.com>
2019-06-03 14:46:16 +0200

    templates: pass correct main package to emitter

--- template/template.go
+++ template/template.go
@@ -76,7 +76,7 @@ func Load(path string, reader Reader, main *scrigo.Package, ctx Context, options
 	// main contains user defined variables, while builtins contains template builtins.
 	// // define something like "emitterBuiltins" in order to avoid converting at every compilation.
 
-	mainFn, globals := compiler.EmitSingle(tree, tci["/main"].TypeInfo, tci["/main"].IndirectVars, alloc)
+	mainFn, globals := compiler.EmitSingle(tree, tci["main"].TypeInfo, tci["main"].IndirectVars, alloc)
 
 	_ = globals // TODO(Gianluca).
 	return &Template{main: main, fn: mainFn}, nil
