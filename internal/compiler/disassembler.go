85c2c6211db41ab7db82ac4d30beead30410ac21
Gianluca Mondini <zapateo@live.it>
2019-06-03 12:40:44 +0200

    Add support for LoadData in disassembler

--- internal/compiler/disassembler.go
+++ internal/compiler/disassembler.go
@@ -419,6 +419,9 @@ func disassembleInstruction(fn *vm.Function, addr uint32) string {
 			s += " " + disassembleOperand(fn, b, vm.Interface, false)
 		}
 		s += " " + disassembleOperand(fn, c, vm.Int, false)
+	case vm.OpLoadData:
+		s += " " + strconv.Itoa(int(a)<<8|int(uint8(b)))
+		s += " " + disassembleOperand(fn, c, vm.Func, false)
 	case vm.OpLoadNumber:
 		if a == 0 {
 			s += " int"
@@ -742,6 +745,8 @@ var operationName = [...]string{
 	vm.OpLeftShift32: "LeftShift32",
 
 	vm.OpLen: "Len",
+
+	vm.OpLoadData: "LoadData",
 
 	vm.OpLoadNumber: "LoadNumber",
 
