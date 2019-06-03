e4050f7c94c042e627162e553029290b3bf6e216
Gianluca Mondini <zapateo@live.it>
2019-06-03 15:50:41 +0200

    Make typechecker replace *ast.Show expression if constant

--- internal/compiler/checker_statements.go
+++ internal/compiler/checker_statements.go
@@ -410,7 +410,12 @@ func (tc *typechecker) checkNodes(nodes []ast.Node) {
 
 		case *ast.Show:
 			// TODO (Gianluca): to review.
-			tc.checkExpression(node.Expr)
+			ti := tc.checkExpression(node.Expr)
+			if ti.IsConstant() {
+				new := ast.NewValue(typedValue(ti, ti.Type))
+				tc.replaceTypeInfo(node.Expr, new)
+				node.Expr = new
+			}
 			tc.terminating = false
 
 		case *ast.ShowMacro:
