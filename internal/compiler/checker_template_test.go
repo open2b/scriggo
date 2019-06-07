24ec9b20fb8ba1f67234f0852a061b02eef8f7bb
Gianluca Mondini <zapateo@live.it>
2019-06-07 08:42:08 +0200

    compiler/checker: test typechecking of templates

---
+++ internal/compiler/checker_template_test.go
@@ -0,0 +1,53 @@
+package compiler_test
+
+import (
+	"scriggo/internal/compiler"
+	"scriggo/internal/compiler/ast"
+	"scriggo/template"
+	"strings"
+	"testing"
+)
+
+var templateCases = map[string]string{
+
+	// Misc.
+	`Just test`: ok,
+	`{{ a }}`:   "undefined: a",
+
+	// Macro definitions.
+	`{% macro M %}{% end %}`:                   ok,
+	`{% macro M() %}{% end %}`:                 ok,
+	`{% macro M(a, b int) %}{% end %}`:         ok,
+	`{% macro M(int, int, string) %}{% end %}`: ok,
+	`{% macro M(a) %}{% end %}`:                "undefined: a",
+
+	// Show macro.
+	`{% macro M %}{% end %}         {% show M %}`:      ok,
+	`{% macro M %}{% end %}         {% show M(1) %}`:   "too many arguments in call to M\n\thave (number)\n\twant ()",
+	`{% macro M(int) %}{% end %}    {% show M("s") %}`: "cannot use \"s\" (type string) as type int in argument to M",
+
+	// "{% macro M %}{% end %}    {% show M() %}":  ok, // TODO(Gianluca): See issue #136.
+}
+
+const ok = ""
+
+func TestTemplate(t *testing.T) {
+	for src, expected := range templateCases {
+		t.Run(src, func(t *testing.T) {
+			r := template.MapReader{"/index.html": []byte(src)}
+			tree, err := compiler.ParseTemplate("/index.html", r, nil, ast.ContextText)
+			if err != nil {
+				t.Fatalf("parsing error: %s", err)
+			}
+			_, err = compiler.Typecheck(&compiler.Options{}, tree, nil, nil)
+			switch {
+			case expected == "" && err != nil:
+				t.Fatalf("unexpected error: %q", err)
+			case expected != "" && err == nil:
+				t.Fatalf("expecting error %q, got nothing", expected)
+			case expected != "" && err != nil && !strings.Contains(err.Error(), expected):
+				t.Fatalf("expecting error %q, got %q", expected, err.Error())
+			}
+		})
+	}
+}
