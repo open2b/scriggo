18003b000064470edaa8676313e999a21f25dcbf
Gianluca Mondini <zapateo@live.it>
2019-06-20 11:33:11 +0200

    scriggob: make imports generator working with scriggob cli interface

--- scriggob/main.go
+++ scriggob/main.go
@@ -41,32 +41,8 @@ func goImports(path string) error {
 	return nil
 }
 
-func usage() string {
-	return "scriggo-generate imports-file variable-name [GOOS[,GOOS[..]]]"
-}
-
-func main() {
-
-	// TODO(Gianluca): check if `go generate` version has the same GOROOT used
-	// in package lookup. If not, panic.
-
-	flag.Parse()
-	args := flag.Args()
-	var gooss []string
-	switch len(args) {
-	case 2:
-		gooss = []string{os.Getenv("GOOS")}
-	case 3:
-		gooss = strings.Split(args[2], ",")
-	default:
-		printErrorAndQuit(usage())
-	}
-	importsFile := flag.Arg(0)
-	customVariableName := flag.Arg(1)
-	if importsFile == "" || customVariableName == "" {
-		printErrorAndQuit(usage())
-	}
-	src, err := ioutil.ReadFile(importsFile)
+func extractImportsFromFile(filepath string) ([]string, string) {
+	src, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		panic(err)
 	}
@@ -74,44 +50,116 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	for _, goos := range gooss {
-		packages := []string{}
-		if len(tree.Nodes) != 1 {
-			printErrorAndQuit("imports file must be a package definition")
-		}
-		pkg, ok := tree.Nodes[0].(*ast.Package)
+	packages := []string{}
+	if len(tree.Nodes) != 1 {
+		printErrorAndQuit("imports file must be a package definition")
+	}
+	pkg, ok := tree.Nodes[0].(*ast.Package)
+	if !ok {
+		printErrorAndQuit("imports file must be a package definition")
+	}
+	for _, n := range pkg.Declarations {
+		imp, ok := n.(*ast.Import)
 		if !ok {
-			printErrorAndQuit("imports file must be a package definition")
+			// TODO(Gianluca):
+			printErrorAndQuit(fmt.Errorf("only imports are allowed in imports file %s", filepath))
 		}
-		for _, n := range pkg.Declarations {
-			imp, ok := n.(*ast.Import)
-			if !ok {
-				printErrorAndQuit(fmt.Errorf("only imports are allowed in imports file %s", importsFile))
+		packages = append(packages, imp.Path)
+	}
+	return packages, pkg.Name
+}
+
+func commandLineError(err string) {
+	fmt.Printf("Command line error: %s\n", err)
+	fmt.Println("Use -help to get help on scriggb usage")
+	os.Exit(1)
+}
+
+func main() {
+
+	// TODO(Gianluca): check if `go generate` version has the same GOROOT used
+	// in package lookup. If not, panic.
+
+	flag.Usage = func() {
+		u := `
+	Usage:   scriggob [OPTIONS] MODE INPUT
+
+Available modes
+---------------
+
+	MODE = "imports" | "sources" | "build"
+
+		imports
+			Generate import file starting from SOURCE putting result into DIR
+			Require "-variable-name" argument.
+		sources
+			Populate DIR with an new intepreter source code which can execute code using the same imports as SOURCE
+		build
+			Same as "sources" but also build intepreter
+
+Options
+-------
+	`
+		fmt.Fprint(os.Stderr, u)
+		flag.PrintDefaults()
+	}
+
+	gooses := flag.String("GOOSs", "", "Target GOOSs (separated by commas). Default to current GOOS")
+	variableName := flag.String("variable-name", "cd", "Custom variable name. Only effective when running in \"imports\" mode")
+
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		commandLineError("a mode must be specified")
+	}
+
+	var gooss []string
+	if *gooses == "" {
+		gooss = []string{os.Getenv("GOOS")}
+	} else {
+		gooss = strings.Split(*gooses, ",")
+	}
+
+	if len(flag.Args()) == 1 {
+		commandLineError("INPUT has not been passed as argument")
+	}
+	importsFile := flag.Arg(1)
+
+	packages, pkgName := extractImportsFromFile(importsFile)
+	if pkgName == "" {
+		panic("pkg name must be specified by command line")
+	}
+
+	switch flag.Arg(0) {
+	case "imports":
+		if *variableName == "" {
+			commandLineError("a custom variable name must be specified when using scriggob in \"imports\" mode")
+		}
+		for _, goos := range gooss {
+			out := generatePackages(packages, importsFile, *variableName, pkgName, goos)
+			importsFileBase := filepath.Base(importsFile)
+			importsFileBaseWithoutExtension := strings.TrimSuffix(importsFileBase, filepath.Ext(importsFileBase))
+			newBase := importsFileBaseWithoutExtension + "_generated" + "_" + goos + filepath.Ext(importsFileBase)
+			outPath := filepath.Join(filepath.Dir(importsFile), newBase)
+			f, err := os.Create(outPath)
+			if err != nil {
+				printErrorAndQuit(err)
 			}
-			packages = append(packages, imp.Path)
+			_, err = f.WriteString(out)
+			if err != nil {
+				printErrorAndQuit(err)
+			}
+			err = goImports(outPath)
+			if err != nil {
+				printErrorAndQuit(err)
+			}
 		}
-
-		out := generatePackages(packages, importsFile, customVariableName, pkg.Name, goos)
-
-		importsFileBase := filepath.Base(importsFile)
-		importsFileBaseWithoutExtension := strings.TrimSuffix(importsFileBase, filepath.Ext(importsFileBase))
-		newBase := importsFileBaseWithoutExtension + "_generated" + "_" + goos + filepath.Ext(importsFileBase)
-
-		outPath := filepath.Join(filepath.Dir(importsFile), newBase)
-
-		f, err := os.Create(outPath)
-		if err != nil {
-			printErrorAndQuit(err)
-		}
-		_, err = f.WriteString(out)
-		if err != nil {
-			printErrorAndQuit(err)
-		}
-		err = goImports(outPath)
-		if err != nil {
-			printErrorAndQuit(err)
-		}
+	case "sources":
+		panic("TODO: not implemented") // TODO(Gianluca): to implement.
+	case "build":
+		panic("TODO: not implemented") // TODO(Gianluca): to implement.
+	default:
+		commandLineError(fmt.Sprintf("mode %q is not valid", flag.Arg(0)))
 	}
 
 }
