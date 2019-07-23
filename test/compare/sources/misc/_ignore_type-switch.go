5398a89a4d0b8e7213799aaaa3aac537f17a7e35
Marco <gazerro@open2b.com>
2019-07-23 20:17:13 +0200

    test/compare: add a failing test on type switch

---
+++ test/compare/sources/misc/_ignore_type-switch.go
@@ -0,0 +1,43 @@
+//+build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var v interface{} = 5
+
+	switch v.(type) {
+	case int:
+		fmt.Println(v)
+	}
+
+	switch a := v.(type) {
+	case int:
+		fmt.Println(a, v)
+	}
+
+	switch a := 3; v.(type) {
+	case int:
+		fmt.Println(a, v)
+	}
+
+	switch a := 3; b := v.(type) {
+	case int:
+		fmt.Println(a, b, v)
+	}
+
+	switch v := interface{}(1); b := v.(type) {
+	case int:
+		fmt.Println(b, v)
+	}
+
+	switch a := interface{}(3); a := a.(type) {
+	case int:
+		fmt.Println(a)
+	}
+
+}
