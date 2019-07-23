4ba7a22cdc02c5d4e3694a3c8abe56e925b0e4b9
Gianluca Mondini <zapateo@live.it>
2019-07-23 16:52:20 +0200

    test: add a test for type assertions

--- test/program_test.go
+++ test/program_test.go
@@ -123,6 +123,22 @@ var stmtTests = []struct {
 }{
 
 	{
+		name: "Type assertions",
+		src: `package main
+
+		import "fmt"
+		
+		func main() {
+			fmt.Print(interface{}(1).(int))
+			i := interface{}(2).(int)
+			fmt.Print(i)
+			fmt.Print(interface{}(5).(int) * interface{}(7).(int))
+		}
+		`,
+		out: `1235`,
+	},
+
+	{
 		name: "Issue #175",
 		src: `package main
 
