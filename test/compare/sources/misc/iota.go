1d0a34204b562f7bbcecb42f236cf9e21c4fe02b
Gianluca Mondini <zapateo@live.it>
2019-07-24 15:49:22 +0200

    test/compare: test constant declarations with iota

---
+++ test/compare/sources/misc/iota.go
@@ -0,0 +1,49 @@
+//+build ignore
+
+package main
+
+import "fmt"
+
+const c1 = iota
+
+const (
+	c2 = iota
+	c3 = iota
+)
+const (
+	c4 = iota
+	c5
+)
+const (
+	c6 = (iota + 1) * 20
+	c7 = iota * 30
+	c8
+)
+
+const (
+	c9  float64 = (iota * 3) + iota
+	c10 float32 = (iota * 3) + iota
+	c11
+	c12
+)
+
+func main() {
+	fmt.Print(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12)
+	{
+		const c1 = iota
+		const (
+			c2 = iota
+			c3 = iota
+		)
+		const (
+			c4 = iota
+			c5
+		)
+		const (
+			c6 = (iota + 1) * 20
+			c7 = iota * 30
+			c8
+		)
+		fmt.Print(c1, c2, c3, c4, c5, c6, c7, c8)
+	}
+}
