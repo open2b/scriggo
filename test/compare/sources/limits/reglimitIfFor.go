d1aaff68f766dee6d634685c0655ecb0e58d17e8
Gianluca Mondini <zapateo@live.it>
2019-07-30 12:43:02 +0200

    test/compare: add tests on register allocation limit

---
+++ test/compare/sources/limits/reglimitIfFor.go
@@ -0,0 +1,995 @@
+//+build ignore
+
+package main
+
+func main() {
+
+	// For statement.
+	for i := 0; i < 500; i++ {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+
+	// If statements.
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+	if true {
+		a := 10
+		b := 20
+		c := a + b
+		_ = c + a
+	}
+
+}
