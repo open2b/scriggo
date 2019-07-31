db17d72caf06c090faf9bf2024ab10b33101bfdc
Gianluca Mondini <zapateo@live.it>
2019-07-31 14:44:58 +0200

    Update/add some tests

--- test/compare/sources/go-tests/const2.go
+++ test/compare/sources/go-tests/const2.go
@@ -1,33 +1,21 @@
-// run
+// errorcheck
 
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Verify that large integer constant expressions cause overflow.
+// Does not compile.
+
 package main
 
-import "fmt"
-
 const (
-	A int = 1 << (1 << iota)
-	B
-	C
-	D
-	E
+	A int = 1
+	B byte;	// ERROR "unexpected semicolon, expecting expression"
 )
 
-func main() {
-	s := fmt.Sprintf("%v %v %v %v %v", A, B, C, D, E)
-	fmt.Println(s)
-	if s != "2 4 16 256 65536" {
-		println("type info didn't propagate in const: got", s)
-		panic("fail")
-	}
-	x := uint(5)
-	y := float64(uint64(1) << x) // used to fail to compile
-	fmt.Println(x, y)
-	if y != 32 {
-		println("wrong y", y)
-		panic("fail")
-	}
-}
+const LargeA = 1000000000000000000
+const LargeB = LargeA * LargeA * LargeA
+const LargeC = LargeB * LargeB * LargeB // GC_ERROR "constant multiplication overflow"
+
+const AlsoLargeA = LargeA << 400 << 400 >> 400 >> 400 // GC_ERROR "constant shift overflow"
