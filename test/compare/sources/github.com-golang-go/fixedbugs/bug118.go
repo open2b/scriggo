8a6f1b34c9cc59eca3763b20d2d86dd96f13725c
Gianluca Mondini <zapateo@live.it>
2019-08-01 15:31:29 +0200

    test/compare/sources: add go tests

---
+++ test/compare/sources/github.com-golang-go/fixedbugs/bug118.go
@@ -0,0 +1,17 @@
+// skip : statement 'select' not implemented https://github.com/open2b/scriggo/issues/109
+
+// compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bug118
+
+func Send(c chan int) int {
+	select {
+	default:
+		return 1;
+	}
+	return 2;
+}
