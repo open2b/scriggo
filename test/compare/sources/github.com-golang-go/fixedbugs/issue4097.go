04059c40fb81c3bf3c24324aaf43b8680b36895d
Gianluca Mondini <zapateo@live.it>
2019-08-01 15:44:27 +0200

    test/compare: add go tests

---
+++ test/compare/sources/github.com-golang-go/fixedbugs/issue4097.go
@@ -0,0 +1,13 @@
+// skip : constant depending on variable https://github.com/open2b/scriggo/issues/206
+
+// errorcheck
+
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package foo
+
+var s [][10]int
+const m = len(s[len(s)-1]) // ERROR "is not a constant|is not constant" 
+
