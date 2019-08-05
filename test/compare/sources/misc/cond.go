a1bc55e6535dad70c18524980c61b102a46f0ee5
Gianluca Mondini <zapateo@live.it>
2019-08-05 12:30:08 +0200

    Test conditions where one of the operands is nil

--- test/compare/sources/misc/cond.go
+++ test/compare/sources/misc/cond.go
@@ -8,7 +8,7 @@ func main() {
 
 	// Condition is a boolean constant.
 	if true {
-		fmt.Print("true")
+		fmt.Println("true")
 	} else {
 		panic("error")
 	}
@@ -16,18 +16,46 @@ func main() {
 	if false {
 		panic("error")
 	} else {
-		fmt.Print("false")
+		fmt.Println("false")
 	}
 
 	if 43 == 43 {
-		fmt.Print("43 == 43")
+		fmt.Println("43 == 43")
 	} else {
 		panic("error")
 	}
 
 	if !true == (56 > 100) {
-		fmt.Print("!true == 56 > 100")
+		fmt.Println("!true == 56 > 100")
 	} else {
 		panic("error")
+	}
+
+	// Binary operations where one of the operands is nil.
+	vNil := []int(nil)
+	if vNil == nil {
+		fmt.Println("vNil == nil")
+	}
+	if vNil != nil {
+		panic("vNil != nil")
+	}
+	if nil == vNil {
+		fmt.Println("nil == vNil")
+	}
+	if nil != vNil {
+		panic("nil != vNil")
+	}
+	vNotNil := []int{}
+	if vNotNil == nil {
+		panic("vNotNil == nil")
+	}
+	if vNotNil != nil {
+		fmt.Println("vNotNil != nil")
+	}
+	if nil == vNotNil {
+		panic("nil == vNotNil")
+	}
+	if nil != vNotNil {
+		fmt.Println("nil != vNotNil")
 	}
 }
