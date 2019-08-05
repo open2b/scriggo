631ae23d39d2e94a6cc1dd9ac502ba503e508c0e
Gianluca Mondini <zapateo@live.it>
2019-08-05 16:30:16 +0200

    compiler/emitter: support 'var' declarations with blank identifier
    Closes #258.

--- internal/compiler/emitter.go
+++ internal/compiler/emitter.go
@@ -1145,15 +1145,19 @@ func (em *emitter) emitNodes(nodes []ast.Node) {
 		case *ast.Var:
 			addresses := make([]address, len(node.Lhs))
 			for i, v := range node.Lhs {
-				staticType := em.ti(v).Type
-				if em.indirectVars[v] {
-					varr := -em.fb.newRegister(reflect.Interface)
-					em.fb.bindVarReg(v.Name, varr)
-					addresses[i] = em.newAddress(addressIndirectDeclaration, staticType, varr, 0)
+				if isBlankIdentifier(v) {
+					addresses[i] = em.newAddress(addressBlank, reflect.Type(nil), 0, 0)
 				} else {
-					varr := em.fb.newRegister(staticType.Kind())
-					em.fb.bindVarReg(v.Name, varr)
-					addresses[i] = em.newAddress(addressRegister, staticType, varr, 0)
+					staticType := em.ti(v).Type
+					if em.indirectVars[v] {
+						varr := -em.fb.newRegister(reflect.Interface)
+						em.fb.bindVarReg(v.Name, varr)
+						addresses[i] = em.newAddress(addressIndirectDeclaration, staticType, varr, 0)
+					} else {
+						varr := em.fb.newRegister(staticType.Kind())
+						em.fb.bindVarReg(v.Name, varr)
+						addresses[i] = em.newAddress(addressRegister, staticType, varr, 0)
+					}
 				}
 			}
 			em.assign(addresses, node.Rhs)
--- test/compare/sources/fixedbugs/issue258.go
+++ test/compare/sources/fixedbugs/issue258.go
@@ -1,9 +1,13 @@
-// skip : https://github.com/open2b/scriggo/issues/258
-
 // compile
 
 package main
 
+var _ = "g1"
+var _, _ = "g2", "g3"
+
 func main() {
 	var _ = nil == interface{}(nil)
+	var _, _ = 1, 2
+	_ = 3
+	_, _, _ = 4,5,6
 }
