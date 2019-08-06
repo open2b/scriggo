1f8673e2700dfa051e914827400283cac633da60
Gianluca Mondini <zapateo@live.it>
2019-08-06 09:55:08 +0200

    test/compare/sources: add two skipped tests related to issues

---
+++ test/compare/sources/fixedbugs/issue236.go
@@ -0,0 +1,19 @@
+// skip : array zeroes are not supported https://github.com/open2b/scriggo/issues/236
+
+// run
+
+package main
+
+import "fmt"
+
+func main() {
+	var chars [6]rune
+	chars[0] = 'a'
+
+	var s [10]int
+	fmt.Printf("%#v", s)
+
+	var ai [4]int
+	ai[0] = ai[1]
+	fmt.Println(len(ai))
+}
---
+++ test/compare/sources/fixedbugs/issue262.go
@@ -0,0 +1,12 @@
+// skip : https://github.com/open2b/scriggo/issues/262
+
+// run
+
+package main
+
+import . "fmt"
+
+func main() {
+	p := Println
+	p("Hello!")
+}
