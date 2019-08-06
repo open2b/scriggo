f5aa3c3ef90173ce0bf10ae93c684c75dab8df7c
Gianluca Mondini <zapateo@live.it>
2019-08-06 11:24:54 +0200

    test/compare/sources: add a skipped test on imports for programs

---
+++ test/compare/sources/programimports/namedImports.dir/main.go
@@ -0,0 +1,9 @@
+package main
+
+import aNewName "a"
+import . "b"
+
+func main() {
+	aNewName.A()
+	B()
+}
---
+++ test/compare/sources/programimports/namedImports.go
@@ -0,0 +1,3 @@
+// skip : false positive imported and not used https://github.com/open2b/scriggo/issues/264
+
+// rundir
