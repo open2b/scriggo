5bb417805a6cf310a1cccbaa8e4388a1d2a08742
Marco <gazerro@open2b.com>
2019-08-18 16:17:28 +0200

    compiler/parser: fix the test for issue 248 because wrongly formatted
    Updates #248

--- test/compare/testdata/fixedbugs/issue248.go
+++ test/compare/testdata/fixedbugs/issue248.go
@@ -2,77 +2,48 @@
 
 package main
 
-var (
-	v1 int
+var ( v1 int )
+var ( v2 int = 5 )
+var ( v3 = 5 )
+
+var ( v4 int; )
+var ( v5 int = 5; )
+var ( v6 = 5; )
+
+var ( v7 int
 )
-var (
-	v2 int = 5
+var ( v8 int = 5
 )
-var (
-	v3 = 5
+var ( v9 = 5
 )
 
-var (
-	v4 int
+var ( v10 int;
 )
-var (
-	v5 int = 5
+var ( v11 int = 5;
 )
-var (
-	v6 = 5
+var ( v12 = 5;
 )
 
-var (
-	v7 int
+const ( c1 = 3 )
+const ( c2 int = 3 )
+const ( c3 = iota; c4 )
+
+const ( c5 = 3; )
+const ( c6 int = 3; )
+const ( c7 = iota; c8; )
+
+const ( c9 = 3
 )
-var (
-	v8 int = 5
+const ( c10 int = 3
 )
-var (
-	v9 = 5
+const ( c11 = iota; c12
 )
 
-const (
-	c1 = 3
+const ( c13 = 3;
 )
-const (
-	c2 int = 3
+const ( c14 int = 3;
 )
-const (
-	c3 = iota
-	c4
-)
-
-const (
-	c5 = 3
-)
-const (
-	c6 int = 3
-)
-const (
-	c7 = iota
-	c8
-)
-
-const (
-	c9 = iota
-	c10
-)
-
-const (
-	c11 = iota
-	c12
-)
-
-const (
-	c13 = 3
-)
-const (
-	c14 int = 3
-)
-const (
-	c15 = iota
-	c16
+const ( c15 = iota; c16;
 )
 
 func main() {
