684692f56d02356c7bb5da6ec2193cf08dd36ae5
Gianluca Mondini <zapateo@live.it>
2019-08-20 10:00:42 +0200

    First commit

---
+++ template/urlWriter.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2019 Open2b Software Snc. All rights reserved.
+// https://www.open2b.com
+
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package template
+
+import "io"
+
+type urlWriter struct {
+	path  bool
+	query bool
+	amper bool
+	w     io.Writer
+}
+
+func (w *urlWriter) Write(p []byte) (int, error) {
+	return 0, nil // TODO(Gianluca).
+}
+
+func (w *urlWriter) WriteText(p []byte) (int, error) {
+	return 0, nil // TODO(Gianluca).
+}
+
+func (w *urlWriter) Reset() {
+	// TODO(Gianluca).
+}
