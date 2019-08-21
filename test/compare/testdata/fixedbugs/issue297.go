aeb06f50c65692532c0786dad8075c248f46e401
Marco Gazerro <gazerro@open2b.com>
2019-08-21 14:10:56 +0200

    compiler/parser: fix test

--- test/compare/testdata/fixedbugs/issue297.go
+++ test/compare/testdata/fixedbugs/issue297.go
@@ -4,58 +4,42 @@ package main
 
 var s1 struct{}
 var s2 struct{ a int }
-var s3 struct{ a int }
-var s4 struct {
-	a int
+var s3 struct{ a int; }
+var s4 struct { a int
 }
-var s5 struct {
-	a int
+var s5 struct { a int;
 }
 var s6 struct {
 	a int
 }
 var s7 struct {
-	a int
+	a int;
 }
 
 var s8 struct{ int }
-var s9 struct{ int }
-var s10 struct {
-	int
+var s9 struct{ int; }
+var s10 struct { int
 }
-var s11 struct {
-	int
+var s11 struct { int;
 }
 var s12 struct {
 	int
 }
 var s13 struct {
-	int
+	int;
 }
 
-var s14 struct {
-	a string
+var s14 struct { a string; b int }
+var s15 struct { a string; b int; }
+var s16 struct { a string; b int
+}
+var s17 struct { a string; b int;
+}
+var s18 struct { a string
 	b int
 }
-var s15 struct {
-	a string
-	b int
-}
-var s16 struct {
-	a string
-	b int
-}
-var s17 struct {
-	a string
-	b int
-}
-var s18 struct {
-	a string
-	b int
-}
-var s19 struct {
-	a string
-	b int
+var s19 struct { a string
+	b int;
 }
 
 func main() {
@@ -63,31 +47,28 @@ func main() {
 	var a int
 	_ = a
 
-	{
-	}
+	{ }
 	{ a = 5 }
-	{
-		a = 5
+	{ a = 5; }
+	{ a = 5
+	}
+	{ a = 5;
 	}
 	{
 		a = 5
 	}
 	{
-		a = 5
-	}
-	{
-		a = 5
-	}
-	{
-		a = 5
+		a = 5;
 	}
 
 	_ = []int{}
 	_ = []int{5}
-	_ = []int{5}
+	_ = []int{5, }
 	_ = []int{5, 6}
-	_ = []int{5}
-	_ = []int{5, 6}
+	_ = []int{5,
+	}
+	_ = []int{5, 6,
+	}
 	_ = []int{
 		5,
 	}
@@ -96,33 +77,41 @@ func main() {
 	}
 
 	_ = [2]int{0: 0}
-	_ = [2]int{0: 0, }
 	_ = [2]int{0: 0,
+	}
+	_ = [2]int{0:
+		0,
 	}
 	_ = [2]int{
 		0: 0,
 	}
 	_ = [2]int{0: 0, 1: 1}
-	_ = [2]int{0: 0, 1: 1, }
 	_ = [2]int{0: 0, 1: 1,
+    }
+	_ = [2]int{0: 0, 1:
+		1,
 	}
-	_ = [2]int{
-		0: 0, 1: 1,
+	_ = [2]int{ 0: 0,
+		1: 1,
 	}
 
 	_ = map[int]int{0: 0}
-	_ = map[int]int{0: 0, }
 	_ = map[int]int{0: 0,
+	}
+	_ = map[int]int{0:
+		0,
 	}
 	_ = map[int]int{
 		0: 0,
 	}
 	_ = map[int]int{0: 0, 1: 1}
-	_ = map[int]int{0: 0, 1: 1, }
 	_ = map[int]int{0: 0, 1: 1,
 	}
-	_ = map[int]int{
-		0: 0, 1: 1,
+	_ = map[int]int{0: 0, 1:
+		1,
+	}
+	_ = map[int]int{ 0: 0,
+		1: 1,
 	}
 
 }
