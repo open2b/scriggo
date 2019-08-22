0c400d9361b686efc548c7e3039586278e571408
Gianluca Mondini <zapateo@live.it>
2019-08-22 11:32:37 +0200

    test/compare/testdata: enable a test on type alias declarations

--- test/compare/testdata/misc/types-declarations.go
+++ test/compare/testdata/misc/types-declarations.go
@@ -1,10 +1,10 @@
-// skip : type definition.
+// run
 
 package main
 
 import "fmt"
 
-type GlobalType []int
+type GlobalType = []int
 
 var g GlobalType = GlobalType{1, 2, 3}
 
@@ -13,39 +13,38 @@ func main() {
 	fmt.Println(g)
 
 	{
-		type T int
+		type T = int
 		var t T = T(0)
 		_ = t
 	}
 	{
-		type Int int
-		type S []Int
+		type Int = int
+		type S = []Int
 		var s S = S{Int(0), Int(1)}
 		fmt.Println(s)
 	}
 	{
-		type S struct{}
+		type S = struct{}
 		var s S = S{}
 		fmt.Println(s)
 	}
 	{
-		type S struct{ A, B int }
+		type S = struct{ A, B int }
 		var s S = S{10, 20}
 		fmt.Println(s)
 	}
 
-	// TODO (Gianluca):
-	// {
-	// 	type F1 struct{ A int }
-	// 	type F2 int
-	// 	type S struct {
-	// 		F1 F1
-	// 		F2 F2
-	// 	}
-	// 	var s S = S{
-	// 		F1: F1{A: 100},
-	// 		F2: F2(int(10)),
-	// 	}
-	// 	fmt.Println(s)
-	// }
+	{
+		type F1 = struct{ A int }
+		type F2 = int
+		type S = struct {
+			F1 F1
+			F2 F2
+		}
+		var s S = S{
+			F1: F1{A: 100},
+			F2: F2(int(10)),
+		}
+		fmt.Println(s)
+	}
 }
