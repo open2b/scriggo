ca2edf7cf6bfeed9dbd1cb9a9ada3a8f6b6ef30c
Gianluca Mondini <zapateo@live.it>
2019-08-26 12:13:09 +0200

    Add tests to send

--- test/compare/testdata/select/send.go
+++ test/compare/testdata/select/send.go
@@ -6,15 +6,27 @@ import (
 	"fmt"
 )
 
-func main() {
-
+func test1() {
 	ch := make(chan int, 1)
-	fmt.Println("ch has length", len(ch))
-
+	fmt.Println(len(ch))
 	select {
 	case ch <- 20:
 		fmt.Println("written 20 on ch")
 	}
+	fmt.Println(len(ch))
+}
 
-	fmt.Println("ch has length", len(ch))
+func test2() {
+	fmt.Println("test2")
+	ch := make(chan []int, 2)
+	select {
+	case ch <- []int{1, 2, 3}:
+		fmt.Println("slice written to the channel")
+	}
+	fmt.Println(<-ch)
+}
+
+func main() {
+	test1()
+	test2()
 }
