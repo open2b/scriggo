8db6504ee03bdd9329163845bfd09ccd72fa1404
Gianluca Mondini <zapateo@live.it>
2019-08-28 11:38:21 +0200

    Fix emission of &T{}

--- internal/compiler/emitter.go
+++ internal/compiler/emitter.go
@@ -1936,7 +1936,11 @@ func (em *emitter) _emitExpr(expr ast.Expression, dstType reflect.Type, reg int8
 			case *ast.CompositeLiteral:
 				tmp := em.fb.newRegister(reflect.Ptr)
 				em.fb.emitNew(exprType, tmp)
-				em.emitExprR(expr, exprType, tmp)
+				if exprType.Kind() == reflect.Struct {
+					em.emitExprR(expr, exprType, tmp)
+				} else {
+					em.emitExprR(expr, exprType, -tmp)
+				}
 				em.changeRegister(false, tmp, reg, typ, dstType)
 			default:
 				panic("TODO(Gianluca): not implemented")
