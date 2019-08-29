d09858ea4355079634f61fe0b099189012957c92
Gianluca Mondini <zapateo@live.it>
2019-08-29 14:27:01 +0200

    Improve test on #197

--- test/compare/testdata/fixedbugs/issue197.go
+++ test/compare/testdata/fixedbugs/issue197.go
@@ -9,11 +9,15 @@ import (
 var a = 4382
 
 func main() {
+
+	// Variable (package).
 	{
 		pa1 := &a
 		pa2 := &a
 		fmt.Println(pa1 == pa2)
 	}
+
+	// Variable (local).
 	{
 		A := 10
 		var (
@@ -22,22 +26,35 @@ func main() {
 		)
 		fmt.Println(ptrA == ptrA2)
 	}
+
+	// Slice.
 	{
 		s := []int{1, 2, 3}
 		p1 := &s[0]
 		p2 := &s[0]
 		fmt.Println(p1 == p2)
 	}
+
+	// Arrays.
 	{
 		a := [5]int{1, 2, 3}
 		p1 := &a[0]
 		p2 := &a[0]
 		fmt.Println(p1 == p2)
 	}
+
+	// Struct.
 	{
 		s := struct{ A int }{A: 20}
 		p1 := &s.A
 		p2 := &s.A
 		fmt.Println(p1 == p2)
+	}
+	{
+		s := struct{ A int }{A: 10}
+		p1 := &s.A
+		fmt.Println(s)
+		(*p1) = 42
+		fmt.Println(s)
 	}
 }
