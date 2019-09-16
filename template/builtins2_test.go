d9e3d5120e872e90b8a0532f2114d3a2e1cd7fc9
Marco Gazerro <gazerro@open2b.com>
2019-09-16 09:43:42 +0200

    template: fix a test on Linux and Windows

--- template/builtins2_test.go
+++ template/builtins2_test.go
@@ -11,6 +11,6 @@ package template
 func init() {
 	rendererBuiltinTestsInJavaScriptContext = append(rendererBuiltinTestsInJavaScriptContext,
 		builtinTest{"var t = {{ t }};", "var t = new Date(\"-012365-03-22T15:19:05.123-07:52\");", Vars{"t": Time(testTime4)}},
-		builtinTest{"var t = new Date(\"{{ t }}\");", "var t = new Date(\"-012365-03-22T15:19:05.123-07:52\");", Vars{"t": Time(testTime4)}},
+		builtinTest{"var t = new Date(\"{{ t }}\");", "var t = new Date(\"-12365-03-22 15:19:05.123456789 -0752 LMT\");", Vars{"t": Time(testTime4)}},
 	)
 }
