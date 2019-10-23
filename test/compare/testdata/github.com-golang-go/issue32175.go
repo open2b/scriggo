a9bcc5b4c53bfb0d37ba1e7635151b5e7fd69d53
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-10-23 11:42:28 +0200

    test/compare/testdata: add reference to #278

--- test/compare/testdata/github.com-golang-go/issue32175.go
+++ test/compare/testdata/github.com-golang-go/issue32175.go
@@ -1,4 +1,4 @@
-// skip : panics
+// skip : named return value referenced from closure https://github.com/open2b/scriggo/issues/278
 
 // Copyright 2019 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
