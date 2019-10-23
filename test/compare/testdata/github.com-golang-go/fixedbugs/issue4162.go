4c1020b4d76570f41c837dd6ccc37e35ae04b7cf
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-10-23 11:20:12 +0200

    test/compare/testdata: enable test 'issue4162.go' disabling a part of it
    * See https://github.com/open2b/scriggo/issues/416.

--- test/compare/testdata/github.com-golang-go/fixedbugs/issue4162.go
+++ test/compare/testdata/github.com-golang-go/fixedbugs/issue4162.go
@@ -1,5 +1,3 @@
-// skip : panics the parser
-
 // compile
 
 // Copyright 2013 The Go Authors. All rights reserved.
@@ -8,12 +6,16 @@
 
 // Issue 4162. Trailing commas now allowed in conversions.
 
-package p
+package main
 
 // All these are valid now.
 var (
 	_ = int(1.0,)      // comma was always permitted (like function call)
 	_ = []byte("foo",) // was syntax error: unexpected comma
 	_ = chan int(nil,) // was syntax error: unexpected comma
-	_ = (func())(nil,) // was syntax error: unexpected comma
+
+	// https://github.com/open2b/scriggo/issues/416
+	// _ = (func())(nil,) // was syntax error: unexpected comma
 )
+
+func main() { }
