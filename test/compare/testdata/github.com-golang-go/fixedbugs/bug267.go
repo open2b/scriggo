5883f73d05f39a78d1ff3dd098a97ff2ee43f65a
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-10-28 17:20:46 +0100

    test/compare/testdata: add some tests taken from the Go repository (3)

---
+++ test/compare/testdata/github.com-golang-go/fixedbugs/bug267.go
@@ -0,0 +1,24 @@
+// compile
+
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+type T = []int
+
+var a []bool
+
+func _() {
+	if a[T{42}[0]] {
+	}
+	// if (a[T{42}[0]]) {}  // this compiles
+}
+
+func main() { }
+
+/*
+6g bugs/bug267.go
+bugs/bug267.go:14: syntax error: unexpected {, expecting :
+*/
