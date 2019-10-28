1c720c98165bcab99e3560adc24c4133e3c44822
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-10-28 16:37:23 +0100

    test/compare/testdata: add some tests taken from the Go repository (2)

---
+++ test/compare/testdata/github.com-golang-go/fixedbugs/bug232.go
@@ -0,0 +1,10 @@
+// skip : type definition (see https://github.com/open2b/scriggo/issues/194)
+
+// compile
+
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bug232
+type I interface { X(...int) }
