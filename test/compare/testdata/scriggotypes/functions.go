0e0fb1bd252ff540fc5cfff5dffaa0d57b06ca22
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-04 15:50:55 +0100

    Fix String and Name methods

--- test/compare/testdata/scriggotypes/functions.go
+++ test/compare/testdata/scriggotypes/functions.go
@@ -10,7 +10,12 @@ func Sum(a, b Int) Int {
 	return a + b
 }
 
+type SumFunc func(a, b Int) Int
+
 func main() {
 	var a, b Int = 30, 12
 	fmt.Println(Sum(a, b))
+
+	// var s SumFunc = Sum
+	// fmt.Println(s(20, 30))
 }
