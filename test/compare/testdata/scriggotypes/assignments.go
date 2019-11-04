98b92fecfe27e243a37b534f673954373dc3271e
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-04 11:10:07 +0100

    Start implementing assignment errors

---
+++ test/compare/testdata/scriggotypes/assignments.go
@@ -0,0 +1,25 @@
+// errorcheck
+
+package main
+
+func main() {
+
+	type Int1 int
+	type Int2 int
+
+	var i1 = Int1(42)
+	var i2 = Int2(42)
+
+	_, _ = i1, i2
+
+	i1 = i1 // Int1, Int1
+	i2 = i2 // Int2, Int2
+	i1 = 22 // Int1, untyped constant
+
+	i1 = i2 // ERROR `cannot use i2 (type Int2) as type Int1 in assignment`
+	i2 = i1 // ERROR `cannot use i1 (type Int1) as type Int2 in assignment`
+
+	i1 = i1 // same scriggo type
+	i2 = i2 // same scriggo type
+
+}
