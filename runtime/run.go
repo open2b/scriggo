bfe4b9c031c2fbc529515babc096aec798ae439a
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-05 17:15:32 +0100

    Add all kinds to OpTypify

--- runtime/run.go
+++ runtime/run.go
@@ -1726,8 +1726,37 @@ func (vm *VM) run() (Addr, bool) {
 			if w, ok := t.(Wrapper); ok {
 				var v interface{}
 				switch t.Kind() {
-				case reflect.Int:
+				// TODO: with this implementation internal types are not
+				// converted to external types.
+				case reflect.Bool,
+					reflect.Int,
+					reflect.Int8,
+					reflect.Int16,
+					reflect.Int32,
+					reflect.Int64,
+					reflect.Uint,
+					reflect.Uint8,
+					reflect.Uint16,
+					reflect.Uint32,
+					reflect.Uint64,
+					reflect.Uintptr:
 					v = vm.intk(b, op < 0)
+				case reflect.Float32,
+					reflect.Float64:
+					v = vm.floatk(b, op < 0)
+				case reflect.String:
+					v = vm.stringk(b, op < 0)
+				case reflect.Complex64,
+					reflect.Complex128,
+					reflect.Array,
+					reflect.Chan,
+					reflect.Func,
+					reflect.Interface,
+					reflect.Map,
+					reflect.Ptr,
+					reflect.Slice,
+					reflect.Struct:
+					v = vm.generalk(b, op < 0)
 				default:
 					panic("TODO: not implemented") // TODO(Gianluca): to implement.
 				}
--- test/compare/testdata/types/emptyinterfaceproxy.go
+++ test/compare/testdata/types/emptyinterfaceproxy.go
@@ -9,13 +9,29 @@ import "fmt"
 func main() {
 
 	{
+
+		// Defined types.
+
 		var ok bool
+		
 		type Int int
 		type String string
+		type SliceByte []byte
+
 		var i1 interface{} = Int(4)
 		_, ok = i1.(Int)    ; fmt.Println(ok) // true
 		_, ok = i1.(String) ; fmt.Println(ok) // false
 		_, ok = i1.(int)    ; fmt.Println(ok) // false
+
+		var i2 interface{} = String("hello")
+		_, ok = i2.(Int)    ; fmt.Println(ok) // false
+		_, ok = i2.(String) ; fmt.Println(ok) // true
+		_, ok = i2.(int)    ; fmt.Println(ok) // false
+
+		// var i3 interface{} = SliceByte([]byte{})
+		// _, ok = i2.(SliceByte) ; fmt.Println(ok) // true
+		// _, ok = i2.([]byte)    ; fmt.Println(ok) // false
 	}
+
 
 }
