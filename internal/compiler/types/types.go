ab185df11d69b0195ec12ba89b59927645ad1952
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-05 11:24:26 +0100

    Fix check of assignability

--- internal/compiler/types/types.go
+++ internal/compiler/types/types.go
@@ -19,9 +19,45 @@ func isDefinedType(t reflect.Type) bool {
 	return t.Name() != ""
 }
 
+// justOneIsDefined returns true only if just one of t1 and t2 is a defined
+// type.
+func justOneIsDefined(t1, t2 reflect.Type) bool {
+	return (isDefinedType(t1) && !isDefinedType(t2)) || (!isDefinedType(t1) && isDefinedType(t2))
+}
+
 func Zero(t reflect.Type) reflect.Value {
 	if st, ok := t.(ScriggoType); ok {
 		return Zero(st.Underlying())
 	}
 	return reflect.Zero(t)
+}
+
+func AssignableTo(x, t reflect.Type) bool {
+
+	T, scriggoType := t.(ScriggoType)
+
+	// T is not a Scriggo type (is a *reflect.rtype), so it's safe to call the
+	// reflect method AssignableTo with T as argument.
+	if !scriggoType {
+		return x.AssignableTo(t)
+	}
+
+	// T is a Scriggo type.
+	// x can be both a Scriggo type or a Go type.
+
+	if x == T {
+		return true
+	}
+
+	// x and T are both defined types but they are not the same: not assignable.
+	if isDefinedType(x) && isDefinedType(T) {
+		return false
+	}
+
+	if justOneIsDefined(x, T) {
+		x.AssignableTo(T.Underlying())
+	}
+
+	// TODO.
+	return false
 }
