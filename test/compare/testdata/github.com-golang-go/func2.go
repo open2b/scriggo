54f022be93957873615a0568ce33ff919c6a28ab
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-06 15:20:19 +0100

    Update tests on type definition

--- test/compare/testdata/github.com-golang-go/func2.go
+++ test/compare/testdata/github.com-golang-go/func2.go
@@ -1,6 +1,4 @@
-// skip : type definition (see https://github.com/open2b/scriggo/issues/194)
-
-// compile
+// skip : missing function body
 
 // Copyright 2009 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
--- test/compare/testdata/github.com-golang-go/func3.go
+++ test/compare/testdata/github.com-golang-go/func3.go
@@ -1,4 +1,4 @@
-// skip : type definition (see https://github.com/open2b/scriggo/issues/194)
+// skip : missing function body
 
 // errorcheck
 
