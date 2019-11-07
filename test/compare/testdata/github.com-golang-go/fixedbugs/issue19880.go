b282e4f5352aeb43efeea82c1b05172b118259ac
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-07 08:44:21 +0100

    Enable/update tests on type definition

--- test/compare/testdata/github.com-golang-go/fixedbugs/issue19880.go
+++ test/compare/testdata/github.com-golang-go/fixedbugs/issue19880.go
@@ -1,12 +1,10 @@
-// skip : type definition (see https://github.com/open2b/scriggo/issues/194)
-
 // errorcheck
 
 // Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package p
+package main
 
 type T struct {
 	f [1]int
@@ -20,3 +18,5 @@ func b() {
 	var v [len(T{}.f)]int // ok
 	_ = v
 }
+
+func main() { }
