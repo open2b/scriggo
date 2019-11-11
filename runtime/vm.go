48b642a6bef3092b2c407d22b231dbeba3d9667b
Marco Gazerro <gazerro@open2b.com>
2019-11-11 16:56:54 +0100

    runtime: remove unused unknownParameter value

--- runtime/vm.go
+++ runtime/vm.go
@@ -1042,7 +1042,6 @@ const (
 	funcParameter      = parameterKind(reflect.Func)
 	interfaceParameter = parameterKind(reflect.Interface)
 	structParameter    = parameterKind(reflect.Struct)
-	unknownParameter   = 254 // https://github.com/open2b/scriggo/issues/390
 	envParameter       = 255
 )
 
