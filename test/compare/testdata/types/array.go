0d24864ed4e8e7a6f4544f0b4874f76e0e570c2c
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-14 15:07:15 +0100

    Improve test 'types/array.go'

--- test/compare/testdata/types/array.go
+++ test/compare/testdata/types/array.go
@@ -8,12 +8,51 @@ type A1 [3]Int
 type A2 [56]A1
 
 func main() {
-	var _ A1
-	var _ A2
-	var _ [5]A1
-	var _ [20][]A2
-	var _ [2]Int
-	var _ [2]map[Int]int
-	var _ [43][][][][][]int
-	var _ [43][][][][][]Int
+	{
+		var a A1
+		e := a[0]
+		_ = e
+	}
+	{
+		var a A2
+		_ = a
+	}
+	{
+		var a [5]A1
+		_ = a
+	}
+	{
+		var a [20][]A2
+		_ = a
+	}
+	{
+		var a [2]Int
+		_ = a
+	}
+	{
+		var a [2]map[Int]int
+		e := a[0][0]
+		_ = e
+	}
+	{
+		var a [43][][][][][]int
+		_ = a
+	}
+	{
+		var a [43][][][][][]Int
+		_ = a
+		a = [43][][][][][]Int{}
+	}
+	{
+		var a [4][10][32][20]Int
+		_ = a
+	}
+	{
+		var a [4][10][32][20]A1
+		_ = a
+	}
+	{
+		var a [4][10][32][20]A2
+		_ = a
+	}
 }
