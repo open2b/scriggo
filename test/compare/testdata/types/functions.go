600315fe6dcc3963ac4865821b8dd99d35a58380
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-14 14:34:55 +0100

    Improve test 'types/functions.go'

--- test/compare/testdata/types/functions.go
+++ test/compare/testdata/types/functions.go
@@ -1,4 +1,4 @@
-// compile
+// run
 
 package main
 
@@ -14,10 +14,10 @@ type SumFunc func(a, b Int) Int
 
 func main() {
 	var a, b Int = 30, 12
-	fmt.Println(Sum(a, b))
+	fmt.Println(int(Sum(a, b)))
 
-	// var s SumFunc = Sum
-	// fmt.Println(s(20, 30))
+	var s SumFunc = SumFunc(Sum)
+	fmt.Println(int(s(20, 30)))
 	var _ func(A Int) = func(A Int) {}
 	type String string
 	var _ func(A Int) String = func(A Int) String { return "" }
