297763d2a0a1b1b1aee6ea21ef7118e4b5c8942a
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-14 14:39:54 +0100

    Improve test 'types/chan.go'

--- test/compare/testdata/types/chan.go
+++ test/compare/testdata/types/chan.go
@@ -11,4 +11,14 @@ func main() {
 	var _ chan String
 	var _ chan<- int
 	var _ chan<- Int
+	var _ chan<- SliceInt
+	
+	c1 := make(chan Int)
+	_ = c1
+
+	c2 := make(chan SliceInt)
+	_ = c2
+
+	c3 := make([](chan [3]String), 10)
+	_ = c3
 }
