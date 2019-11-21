c71b53165b97124c0c92c4d52e77e7e1c20e28c6
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-21 15:41:44 +0100

    Add a skipped test

---
+++ test/compare/testdata/misc/shift2.go
@@ -0,0 +1,16 @@
+// skip
+
+// errorcheck
+
+package main
+
+func main() {
+
+	v := 2
+	_ = v
+
+	var _ float64 = (1 << v)          // ERROR `invalid operation: 1 << v (shift of type float64)`
+	var _ = (1 << v) + (2 << v) + 2.1 // ERROR `invalid operation: 1 << v (shift of type float64)`
+	var _ int8 = (1000000 << v)       // ERROR `constant 1000000 overflows int8`
+
+}
