2f1f89399e7b57be58badcf7a950fb9a44fbb16b
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-22 12:11:02 +0100

    Fix tests

--- test/compare/testdata/misc/shift2.go
+++ test/compare/testdata/misc/shift2.go
@@ -7,15 +7,13 @@ func main() {
 	v := 2
 	_ = v
 
-	var _ float64 = (1 << v)    // ERROR `invalid operation: 1 << v (shift of type float64)`
-	var _ int8 = (1000000 << v) // ERROR `constant 1000000 overflows int8`
-
-	var _ = (9999999999 << v) == rune(10<<v) // ERROR `constant 9999999999 overflows int32`
-
-	var _ = (1 << v) + (2 << v) + 2.1 // ERROR `invalid operation: 1 << v (shift of type float64)`
-
-	var _ = (1 << v) + 1i // ERROR `invalid operation: 1 << v (shift of type complex128)`
-
-	var _ int = (1 << v) + 1i // ERROR `invalid operation: 1 << v (shift of type complex128)`
+	var (
+		_ float64 = (1 << v)                         // ERROR `invalid operation: 1 << v (shift of type float64)`
+		_ int8    = (1000000 << v)                   // ERROR `constant 1000000 overflows int8`
+		_         = (9999999999 << v) == rune(10<<v) // ERROR `constant 9999999999 overflows int32`
+		_         = (1 << v) + (2 << v) + 2.1        // ERROR `invalid operation: 1 << v (shift of type float64)`
+		_         = (1 << v) + 1i                    // ERROR `invalid operation: 1 << v (shift of type complex128)`
+		_ int     = (1 << v) + 1i                    // ERROR `invalid operation: 1 << v (shift of type complex128)`
+	)
 
 }
