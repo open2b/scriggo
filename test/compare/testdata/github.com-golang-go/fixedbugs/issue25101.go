b5c5cef3746ed5d00f51c7d4bfb8722d8477831e
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-25 16:40:07 +0100

    test/compare/testdata: add tests from the Go repository

---
+++ test/compare/testdata/github.com-golang-go/fixedbugs/issue25101.go
@@ -0,0 +1,18 @@
+// skip : interface definition
+
+// compile
+
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Indexed export format must not crash when writing
+// the anonymous parameter for m.
+
+package p
+
+var x interface {
+	m(int)
+}
+
+var M = x.m
