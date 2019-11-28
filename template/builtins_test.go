58cf419ae1e31fd16a134788d71f9b813f31519f
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-11-28 15:14:00 +0100

    template: disable some tests that pass accidentally
    * See https://github.com/open2b/scriggo/issues/235.

--- template/builtins_test.go
+++ template/builtins_test.go
@@ -224,11 +224,12 @@ var rendererBuiltinTestsInHTMLContext = []builtinTest{
 	{"{{ replaceAll(`abcbcba`, `b`, `e`) }}", "aececea", nil},
 
 	// reverse
-	{"{% s := reverse(s) %}{{ sprint(s) }}", "[]", Vars{"s": []int(nil)}},
-	{"{% s := reverse(s) %}{{ sprint(s) }}", "[]", Vars{"s": []int{}}},
-	{"{% s := reverse(s) %}{{ sprint(s) }}", "[1]", Vars{"s": []int{1}}},
-	{"{% s := reverse(s) %}{{ sprint(s) }}", "[2 1]", Vars{"s": []int{1, 2}}},
-	{"{% s := reverse(s) %}{{ sprint(s) }}", "[3 2 1]", Vars{"s": []int{1, 2, 3}}},
+	// These tests pass accidentally. See https://github.com/open2b/scriggo/issues/235.
+	// {"{% s := reverse(s) %}{{ sprint(s) }}", "[]", Vars{"s": []int(nil)}},
+	// {"{% s := reverse(s) %}{{ sprint(s) }}", "[]", Vars{"s": []int{}}},
+	// {"{% s := reverse(s) %}{{ sprint(s) }}", "[1]", Vars{"s": []int{1}}},
+	// {"{% s := reverse(s) %}{{ sprint(s) }}", "[2 1]", Vars{"s": []int{1, 2}}},
+	// {"{% s := reverse(s) %}{{ sprint(s) }}", "[3 2 1]", Vars{"s": []int{1, 2, 3}}},
 
 	// round
 	{"{{ round(0) }}", "0", nil},
