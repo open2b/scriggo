8e30f309fec0baee04d29dbda067266abaae6de9
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2019-12-02 12:34:52 +0100

    Update

--- internal/compiler/checker_statements.go
+++ internal/compiler/checker_statements.go
@@ -775,6 +775,173 @@ nodesLoop:
 
 }
 
+func (tc *typechecker) checkImport(d *ast.Import, imports PackageLoader, pkgInfos map[string]*PackageInfo, packageLevel bool) error {
+
+	// Get the package info.
+	importedPkg := &PackageInfo{}
+	if d.Tree == nil {
+		// Predefined package.
+		if packageLevel {
+			pkg, err := imports.Load(d.Path)
+			if err != nil {
+				return tc.errorf(d, "%s", err)
+			}
+			predefinedPkg := pkg.(predefinedPackage)
+			if predefinedPkg.Name() == "main" {
+				return tc.programImportError(d)
+			}
+			declarations := predefinedPkg.DeclarationNames()
+			importedPkg.Declarations = make(map[string]*TypeInfo, len(declarations))
+			for n, d := range toTypeCheckerScope(predefinedPkg, 0, tc.opts) {
+				importedPkg.Declarations[n] = d.t
+			}
+			importedPkg.Name = predefinedPkg.Name()
+		}
+	} else {
+		if packageLevel {
+			// Not predefined package.
+			var err error
+			if tc.opts.SyntaxType == TemplateSyntax {
+				err := tc.templatePageToPackage(d.Tree, d.Tree.Path)
+				if err != nil {
+					return err
+				}
+				if d.Tree.Nodes[0].(*ast.Package).Name == "main" {
+					return tc.programImportError(d)
+				}
+				err = checkPackage(d.Tree.Nodes[0].(*ast.Package), d.Tree.Path, nil, pkgInfos, tc.opts, tc.globalScope)
+				if err != nil {
+					return err
+				}
+			} else {
+				if d.Tree.Nodes[0].(*ast.Package).Name == "main" {
+					return tc.programImportError(d)
+				}
+				err = checkPackage(d.Tree.Nodes[0].(*ast.Package), d.Tree.Path, imports, pkgInfos, tc.opts, tc.globalScope)
+				if err != nil {
+					return err
+				}
+			}
+			importedPkg = pkgInfos[d.Tree.Path]
+		}
+	}
+
+	// Check the import itself.
+	if tc.opts.SyntaxType == TemplateSyntax {
+		if !packageLevel {
+			if d.Ident != nil && d.Ident.Name == "_" {
+				return nil
+			}
+			err := tc.templatePageToPackage(d.Tree, d.Tree.Path)
+			if err != nil {
+				return err
+			}
+			pkgInfos := map[string]*PackageInfo{}
+			if d.Tree.Nodes[0].(*ast.Package).Name == "main" {
+				return tc.programImportError(d)
+			}
+			err = checkPackage(d.Tree.Nodes[0].(*ast.Package), d.Path, nil, pkgInfos, tc.opts, tc.globalScope)
+			if err != nil {
+				return err
+			}
+			// TypeInfos of imported packages in templates are
+			// "manually" added to the map of typeinfos of typechecker.
+			for k, v := range pkgInfos[d.Path].TypeInfos {
+				tc.typeInfos[k] = v
+			}
+			importedPkg = pkgInfos[d.Path]
+		}
+		if d.Ident == nil {
+			tc.unusedImports[importedPkg.Name] = nil
+			for ident, ti := range importedPkg.Declarations {
+				tc.unusedImports[importedPkg.Name] = append(tc.unusedImports[importedPkg.Name], ident)
+				tc.filePackageBlock[ident] = scopeElement{t: ti}
+			}
+			return nil
+		}
+		switch d.Ident.Name {
+		case "_":
+		case ".":
+			tc.unusedImports[importedPkg.Name] = nil
+			for ident, ti := range importedPkg.Declarations {
+				tc.unusedImports[importedPkg.Name] = append(tc.unusedImports[importedPkg.Name], ident)
+				tc.filePackageBlock[ident] = scopeElement{t: ti}
+			}
+		default:
+			tc.filePackageBlock[d.Ident.Name] = scopeElement{
+				t: &TypeInfo{
+					value:      importedPkg,
+					Properties: PropertyIsPackage | PropertyHasValue,
+				},
+			}
+			tc.unusedImports[d.Ident.Name] = nil
+		}
+		return nil
+	}
+	if tc.opts.SyntaxType == ScriptSyntax {
+		pkg, err := tc.predefinedPkgs.Load(d.Path)
+		if err != nil {
+			return tc.errorf(d, "%s", err)
+		}
+		predefinedPkg := pkg.(predefinedPackage)
+		if predefinedPkg.Name() == "main" {
+			return tc.programImportError(d)
+		}
+		decls := predefinedPkg.DeclarationNames()
+		importedPkg = &PackageInfo{}
+		importedPkg.Declarations = make(map[string]*TypeInfo, len(decls))
+		for n, d := range toTypeCheckerScope(predefinedPkg, 0, tc.opts) {
+			importedPkg.Declarations[n] = d.t
+		}
+		importedPkg.Name = predefinedPkg.Name()
+		if d.Ident == nil {
+			tc.filePackageBlock[importedPkg.Name] = scopeElement{t: &TypeInfo{value: importedPkg, Properties: PropertyIsPackage | PropertyHasValue}}
+			tc.unusedImports[importedPkg.Name] = nil
+		} else {
+			switch d.Ident.Name {
+			case "_":
+			case ".":
+				tc.unusedImports[importedPkg.Name] = nil
+				for ident, ti := range importedPkg.Declarations {
+					tc.unusedImports[importedPkg.Name] = append(tc.unusedImports[importedPkg.Name], ident)
+					tc.filePackageBlock[ident] = scopeElement{t: ti}
+				}
+			default:
+				tc.filePackageBlock[d.Ident.Name] = scopeElement{t: &TypeInfo{value: importedPkg, Properties: PropertyIsPackage | PropertyHasValue}}
+				tc.unusedImports[d.Ident.Name] = nil
+			}
+		}
+		return nil
+	}
+
+	if tc.opts.SyntaxType == ProgramSyntax {
+		if d.Ident == nil {
+			tc.filePackageBlock[importedPkg.Name] = scopeElement{t: &TypeInfo{value: importedPkg, Properties: PropertyIsPackage | PropertyHasValue}}
+			tc.unusedImports[importedPkg.Name] = nil
+		} else {
+			switch d.Ident.Name {
+			case "_":
+			case ".":
+				tc.unusedImports[importedPkg.Name] = nil
+				for ident, ti := range importedPkg.Declarations {
+					tc.unusedImports[importedPkg.Name] = append(tc.unusedImports[importedPkg.Name], ident)
+					tc.filePackageBlock[ident] = scopeElement{t: ti}
+				}
+			default:
+				tc.filePackageBlock[d.Ident.Name] = scopeElement{
+					t: &TypeInfo{
+						value:      importedPkg,
+						Properties: PropertyIsPackage | PropertyHasValue,
+					},
+				}
+				tc.unusedImports[d.Ident.Name] = nil
+			}
+		}
+	}
+
+	return nil
+}
+
 // checkReturn type checks a return statement.
 //
 // If the return statement has an expression list and the returning function has
