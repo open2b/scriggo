ad9a6c2cf82454def711bf53589e0e1699586ab9
Marco <gazerro@open2b.com>
2020-01-12 10:33:56 +0100

    test/compare: use math package constants in test

--- test/compare/testdata/misc/constant_negative_zero.go
+++ test/compare/testdata/misc/constant_negative_zero.go
@@ -4,11 +4,6 @@ package main
 
 import "math"
 
-const (
-	MaxFloat64             = 1.797693134862315708145274237317043567981e+308
-	SmallestNonzeroFloat64 = 4.940656458412465441765687928682213723651e-324
-)
-
 func check(f float64) {
 	if f != 0 || math.Signbit(f) {
 		println("BUG: got", f, "want 0.0")
@@ -23,8 +18,8 @@ func main() {
 	check(x)
 	check(y)
 	check(float64(z))
-	check(-SmallestNonzeroFloat64 / MaxFloat64)
-	check(SmallestNonzeroFloat64 / -MaxFloat64)
+	check(-math.SmallestNonzeroFloat64 / math.MaxFloat64)
+	check(math.SmallestNonzeroFloat64 / -math.MaxFloat64)
 	check(x + y)
 	check(x - y)
 	check(x * y)
