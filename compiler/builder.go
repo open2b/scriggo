7ccd95e64feee7587d9d5e509f8a939f96ca63ee
Marco Gazerro <gazerro@open2b.com>
2020-01-15 17:23:52 +0100

    compiler/builder: optimize register allocations in makeGeneralConstant
    Fix #508

--- compiler/builder.go
+++ compiler/builder.go
@@ -390,8 +390,8 @@ func (builder *functionBuilder) makeStringConstant(c string) int8 {
 }
 
 // makeGeneralConstant makes a new general constant, returning it's index.
-//
-// c must be the zero of it's type.
+// The argument c must be nil or must be a complex number or must be the zero
+// value of its type.
 //
 // If the VM's internal representation of c is different from the external, c
 // must always have the external representation. Any conversion, if needed, will
@@ -399,10 +399,21 @@ func (builder *functionBuilder) makeStringConstant(c string) int8 {
 func (builder *functionBuilder) makeGeneralConstant(c interface{}) int8 {
 	// Check if a constant with the same value has already been added to the
 	// general Constants slice.
-	if typ := reflect.TypeOf(c); c != nil && typ.Comparable() {
-		kind := typ.Kind()
+	var t reflect.Type
+	var k reflect.Kind
+	if c != nil {
+		t = reflect.TypeOf(c)
+		k = t.Kind()
+	}
+	if c == nil || k == reflect.Complex64 || k == reflect.Complex128 {
 		for i, v := range builder.fn.Constants.General {
-			if v != nil && reflect.TypeOf(v).Kind() == kind && c == v {
+			if c == v {
+				return int8(i)
+			}
+		}
+	} else {
+		for i, v := range builder.fn.Constants.General {
+			if v != nil && t == reflect.TypeOf(v) {
 				return int8(i)
 			}
 		}
