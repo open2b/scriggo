e208133127821859182b91c3c4fcf88c161845c4
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2020-01-20 09:44:10 +0100

    cmd/scriggo: skip two tests that need to be updated (#546)

--- cmd/scriggo/generate_test.go
+++ cmd/scriggo/generate_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 func Test_renderPackages(t *testing.T) {
+	t.Skip() // TODO:https://github.com/open2b/scriggo/issues/546
 	// NOTE: these tests ignores whitespaces, imports and comments.
 	cases := map[string]struct {
 		sf       *scriggofile
@@ -249,6 +250,7 @@ func _cleanOutput(s string) string {
 }
 
 func Test_parseGoPackage(t *testing.T) {
+	t.Skip() // TODO:https://github.com/open2b/scriggo/issues/546
 	cases := map[string]struct {
 		name  string            // package name.
 		decls map[string]string // package declarations.
