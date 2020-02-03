73aef5204ee9d5fbf0e7f59f7aa65684aeacdcf0
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2020-02-03 15:21:25 +0100

    template: fix package name

--- template/packages.go
+++ template/packages.go
@@ -4,7 +4,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package scritto
+package template
 
 // Package represents a predefined package.
 type Package interface {
