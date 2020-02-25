d959bc4bead595716dae677bbc4f25add25722a2
Gianluca Mondini <Gianluca.Mondini@outlook.it>
2020-02-25 08:55:47 +0100

    compiler: add documentation to compilation

--- compiler/compilation.go
+++ compiler/compilation.go
@@ -6,10 +6,19 @@
 
 package compiler
 
+// A compilation holds the state of a single compilation.
+//
+// This is necessary to store information across compilation of different
+// packages/template pages, where a new type checker is created for every
+// package/template page.
+//
+// Currently the compilation is used only by the typechecker.
+//
 type compilation struct {
 	pkgPathToIndex map[string]int
 }
 
+// newCompilation returns a new compilation.
 func newCompilation() *compilation {
 	return &compilation{
 		pkgPathToIndex: map[string]int{},
