d9056cd1849ddaf7ce9570e82146d9da036190e6
Marco Gazerro <gazerro@open2b.com>
2020-12-01 15:11:14 +0100

    compiler/checker: add a missing header to test file

--- compiler/checker_template_test.go
+++ compiler/checker_template_test.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2019 Open2b Software Snc. All rights reserved.
+// https://www.open2b.com
+
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package compiler
 
 import (
