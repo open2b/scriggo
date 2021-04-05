969d1a2fffb85fcf5b5cb4b298c5a3b2bc7e40b7
Marco <gazerro@open2b.com>
2021-04-05 12:29:43 +0200

    test/bench: add benchmark 'calls'

--- test/bench/main.go
+++ test/bench/main.go
@@ -8,9 +8,12 @@ package main
 
 import (
 	"bytes"
+	"embed"
 	_ "embed"
 	"fmt"
+	"io/fs"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/open2b/scriggo"
@@ -18,8 +21,8 @@ import (
 	"golang.org/x/tools/txtar"
 )
 
-//go:embed micro.txtar
-var tests []byte
+//go:embed micro.txtar *_test/*
+var tests embed.FS
 
 type programToRun struct {
 	name string
@@ -51,15 +54,65 @@ func main() {
 }
 
 func build() ([]programToRun, error) {
-	arch := txtar.Parse(tests)
+	micro, err := tests.ReadFile("micro.txtar")
+	if err != nil {
+		return nil, err
+	}
+	arch := txtar.Parse(micro)
 	programs := make([]programToRun, len(arch.Files))
 	for i, file := range arch.Files {
 		program, err := scriggo.Build(bytes.NewReader(file.Data), nil, nil)
 		if err != nil {
 			return nil, fmt.Errorf("cannot build %s: %s", file.Name, err)
 		}
-		programs[i].name = file.Name
+		programs[i].name = "Micro"+file.Name
 		programs[i].code = program
 	}
+	_ = fs.WalkDir(tests, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			if path != "." && !strings.HasSuffix(path, "_test") {
+				return fs.SkipDir
+			}
+			return nil
+		}
+		if !strings.Contains(path, "/") {
+			return nil
+		}
+		data, _ := tests.ReadFile(path)
+		program, err := scriggo.Build(bytes.NewReader(data), nil, nil)
+		if err != nil {
+			return fmt.Errorf("cannot build %s: %s", path, err)
+		}
+		name := capitalizeTestName(path[:strings.Index(path, "_test/")])
+		programs = append(programs, programToRun{name: name, code: program})
+		return nil
+	})
 	return programs, nil
+}
+
+// capitalizeTestName capitalizes a test name. Given "call_indirect" returns
+// "CallIndirect". If name is malformed, it returns an empty string.
+func capitalizeTestName(name string) string {
+	var i = 0
+	var n = make([]byte, len(name))
+	for _, c := range name {
+		if 'a' <= c && c <= 'z' {
+			if i == 0 || n[i] == '_' {
+				n[i] = byte(c) - 32
+			} else {
+				n[i] = byte(c)
+			}
+		} else if c == '_' {
+			if i == 0 || n[i] == '_' {
+				return ""
+			}
+			n[i] = byte(c)
+		} else {
+			return ""
+		}
+		if c != '_' {
+			i++
+		}
+	}
+	return string(n[:i])
 }
