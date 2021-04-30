a88fdecb58f03790744010d34a81553ff315d78c
Marco Gazerro <gazerro@open2b.com>
2021-04-30 11:55:32 +0200

    cmd/scriggo: remove struct tags from the list of limitations
    It has been implemented in commit
    0681f160100ff6242f5c9abfc3186ae487536104.

--- cmd/scriggo/help.go
+++ cmd/scriggo/help.go
@@ -316,7 +316,6 @@ Limitations
     * importing the "runtime" package from Scriggo (issue #524)
     * labeled continue and break statements (issue #83)
     * struct type declarations with implicit fields (issue #367)
-    * struct type declarations with tags (issue #61)
     * some kinds of pointer shorthands (issue #383)
     * compilation of non-main packages without importing them (issue #521)
 
