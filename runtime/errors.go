1cee984c7606ba3e58ab6505f44e429d600157c9
Marco <gazerro@open2b.com>
2021-06-15 09:08:23 +0200

    runtime: simplify error handling converting from []T to *[N]T
    After CL golang/go@0fd20ed the error message returned by reflect is the
    same as the one returned by the runtime. So this change simplifies the
    error handling.

--- runtime/errors.go
+++ runtime/errors.go
@@ -160,10 +160,7 @@ func (vm *VM) convertPanic(msg interface{}) error {
 		}
 	case OpConvert:
 		if err, ok := msg.(string); ok && strings.HasPrefix(err, "reflect: cannot convert slice with length") {
-			in := vm.fn.Body[vm.pc-1]
-			sl := vm.general(in.A).Len()
-			al := vm.fn.Types[uint8(in.B)].Elem().Len()
-			return vm.newPanic(runtimeError(fmt.Sprintf("runtime error: cannot convert slice with length %d to pointer to array with length %d", sl, al)))
+			return vm.newPanic(runtimeError("runtime error:" + err[len("reflect:"):]))
 		}
 	case OpDivInt, OpDiv, OpRemInt, OpRem:
 		if err, ok := msg.(runtime.Error); ok {
