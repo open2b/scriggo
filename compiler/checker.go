fa1f84bc66a8ff71734aa799f4d9399ad67d155d
Gianluca Mondini <mondini@open2b.com>
2021-06-25 15:56:14 +0200

    review: fix 'thisCurrentName'

--- compiler/checker.go
+++ compiler/checker.go
@@ -278,7 +278,7 @@ func (tc *typechecker) thisIncreaseIndex() {
 
 // thisCurrentName returns the current name of the 'this' identifier.
 func (tc *typechecker) thisCurrentName() string {
-	return "$_this_%d" + strconv.Itoa(tc.using.currentThisIndex)
+	return "$_this_" + strconv.Itoa(tc.using.currentThisIndex)
 }
 
 // newTypechecker creates a new type checker. A global scope may be provided
