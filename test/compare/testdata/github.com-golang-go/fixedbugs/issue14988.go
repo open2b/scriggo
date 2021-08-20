caf7a27f48a800775c765851d4f20ab97a134220
Gianluca Mondini <mondini@open2b.com>
2021-08-20 17:18:37 +0200

    test/compare/testdata/github.com-golang-go: add 5 tests

---
+++ test/compare/testdata/github.com-golang-go/fixedbugs/issue14988.go
@@ -0,0 +1,16 @@
+// skip : invalid recursive type
+
+// errorcheck
+
+// Copyright 2016 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Issue 14988: defining a map with an invalid forward declaration array
+//              key doesn't cause a fatal.
+
+package main
+
+type m map[k]int; type k [1]m // ERROR "invalid map key type"
+
+func main() { }
