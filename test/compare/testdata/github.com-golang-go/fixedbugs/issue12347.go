4acda7acf0dac55682475a2c7cc3011cc5b34384
Gianluca Mondini <mondini@open2b.com>
2021-08-20 16:21:31 +0200

    test/compare/testdata: add 17 tests from gc

---
+++ test/compare/testdata/github.com-golang-go/fixedbugs/issue12347.go
@@ -0,0 +1,18 @@
+// compile
+
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func f_ssa(x int, p *int) {
+	if false {
+		y := x + 5
+		for {
+			*p = y
+		}
+	}
+}
+
+func main() { }
