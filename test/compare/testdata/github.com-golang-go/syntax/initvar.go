e3abb16dd8654c42eddd00979e39b2ecee75ceeb
Gianluca Mondini <mondini@open2b.com>
2021-08-20 09:32:35 +0200

    test/compare/testdata/github.com-golang-go: add 20 tests

---
+++ test/compare/testdata/github.com-golang-go/syntax/initvar.go
@@ -0,0 +1,17 @@
+// skip : error messages are different from gc
+
+// errorcheck
+
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+func main() {
+	if var x = 0; x < 10 {}    // ERROR "var declaration not allowed in if initializer"
+
+	switch var x = 0; x {}     // ERROR "var declaration not allowed in switch initializer"
+
+	for var x = 0; x < 10; {}  // ERROR "var declaration not allowed in for initializer"
+}
---
+++ test/compare/testdata/github.com-golang-go/typeparam/mdempsky/11.go
@@ -0,0 +1,20 @@
+// skip : investigate
+
+// errorcheck
+
+// Copyright 2021 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Reported by Cuong Manh Le.
+
+package main
+
+type a struct{}
+
+//go:notinheap
+type b a
+
+var _ = (*b)(new(a)) // ERROR "cannot convert"
+
+func main() { }
