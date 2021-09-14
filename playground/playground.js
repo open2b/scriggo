981431c1badc0124d34bc1a0ae20ad7351948b10
Gianluca Mondini <mondini@open2b.com>
2021-09-14 11:22:56 +0200

    playground: show a waiting message while loading 'scriggo.wasm'

--- playground/playground.js
+++ playground/playground.js
@@ -26,9 +26,10 @@
     function fetchAndInstantiate(url, importObject) {
         return fetch(url).then(response =>
             response.arrayBuffer()
-        ).then(bytes =>
-            WebAssembly.instantiate(bytes, importObject)
-        ).then(results =>
+        ).then(bytes => {
+            document.getElementById("Output").innerHTML = "";
+            return WebAssembly.instantiate(bytes, importObject)
+        }).then(results =>
             results.instance
         );
     }
