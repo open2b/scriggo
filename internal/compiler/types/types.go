4285ca908782d42c46aa598b294a9c3be535264b
Gianluca Mondini <mondini@open2b.com>
2021-09-15 17:07:24 +0200

    internal/compiler/types: fix documentation according to 'golint'

--- internal/compiler/types/types.go
+++ internal/compiler/types/types.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// package types implements functions and types to represent and work with
+// Package types implements functions and types to represent and work with
 // Scriggo types.
 package types
 
