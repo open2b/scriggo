798dbeaf4756ce74f7abf5ead5c5f8517c0661e0
Marco Gazerro <gazerro@open2b.com>
2024-12-04 12:57:10 +0100

    internal/runtime: remove unused variable

--- internal/runtime/vm.go
+++ internal/runtime/vm.go
@@ -29,7 +29,6 @@ const stackSize = 512
 var envType = reflect.TypeOf((*native.Env)(nil)).Elem()
 var emptyInterfaceType = reflect.TypeOf(&[]interface{}{nil}[0]).Elem()
 var emptyInterfaceNil = reflect.ValueOf(&[]interface{}{nil}[0]).Elem()
-var stringType = reflect.TypeOf("")
 
 // Converter is implemented by format converters.
 type Converter func(src []byte, out io.Writer) error
