288a7f7917ec657a320bd3bab3c21632eb9845cd
Marco Gazerro <gazerro@open2b.com>
2024-12-11 17:58:18 +0100

    test/compare: add more tests for the `append` builtin

--- test/compare/testdata/program_tests/append.go
+++ test/compare/testdata/program_tests/append.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+
 	s := []int{}
 	fmt.Print(s)
 	s = append(s, []int{1, 2, 3}...)
@@ -14,4 +15,30 @@ func main() {
 	ns := []int{4, 5, 6}
 	s = append(s, ns...)
 	fmt.Print(s)
+	s = append(s, 7, 8, 9, 10)
+	fmt.Print(s)
+
+	b := []byte{}
+	fmt.Print(b)
+	b = append(b, 1)
+	fmt.Print(b)
+	b = append(b, 2, 3, 4)
+	fmt.Print(b)
+	b = append(b, []byte{5, 6, 7, 8}...)
+	fmt.Print(b)
+
+	r := []rune{'a'}
+	fmt.Print(r)
+	r = append(r, []rune{'b', 'c'}...)
+	fmt.Print(r)
+	r = append(r, 'd', 'e', 'f')
+	fmt.Print(r)
+
+	type T struct{}
+	t := []T{}
+	fmt.Print(len(t))
+	t = append(t, T{}, T{})
+	fmt.Print(len(t))
+	t = append(t, []T{{}, {}}...)
+	fmt.Print(len(t))
 }
